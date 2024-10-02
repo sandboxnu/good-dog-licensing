a52628c6f02a896e8023b3752aed6afc17787cea
Jordan Praissman <jordanpraissman@gmail.com>
2024-10-02 19:31:05 -0400

    Started working on seed script

---
+++ apps/db/prisma/seed.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { email: "alice@prisma.io" },
+    update: {},
+    create: {
+      email: "alice@prisma.io",
+      name: "Alice",
+      password: "test",
+    },
+  });
+  console.log({ alice });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
