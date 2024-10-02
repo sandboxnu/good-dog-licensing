a99656bd1dcbb1f78486ad69fff622b2c4497b96
sanjana-singhania <singhania.sa@northeastern.edu>
2024-10-02 19:27:37 -0400

    cleaned up migration files

--- apps/db/prisma/migrations/20241002232629_user_and_session_schemas/migration.sql
+++ apps/db/prisma/migrations/20241002232629_user_and_session_schemas/migration.sql
@@ -26,3 +26,6 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Session_token_key" ON "Session"("token");
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
