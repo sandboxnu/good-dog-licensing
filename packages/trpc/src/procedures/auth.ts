39cd9e736d62ebe402820161514bd672170eba16
tracyyh <huang.tr@northeastern.edu>
2024-10-06 15:43:59 -0400

    fix errors

--- packages/trpc/src/procedures/auth.ts
+++ packages/trpc/src/procedures/auth.ts
@@ -10,24 +10,16 @@ export const signUpProcedure = baseProcedureBuilder
     }),
   )
   .mutation(async ({ ctx, input }) => {
-    const user = await ctx.prisma.user
-      .create({
-        data: {
-          email: input.email,
-          password: input.password,
-        },
-      })
-      .catch((error) => {
-        if (error.code === "P2002") {
-          throw new Error("Email already exists");
-        }
-      });
+    await ctx.prisma.user.create({
+      data: {
+        email: input.email,
+        password: input.password,
+      },
+    });
 
-    if (user) {
-      return {
-        response: `Successfully signed up and logged in as ${user.email}`,
-      };
-    }
+    return {
+      response: `Successfully signed up and logged in as ${input.email}`,
+    };
   });
 
 export const signInProcedure = baseProcedureBuilder
@@ -51,28 +43,21 @@ export const signInProcedure = baseProcedureBuilder
     const date = new Date();
     date.setDate(date.getDate() + 30);
 
-    const session = await ctx.prisma.session
-      .create({
-        data: {
-          user: {
-            connect: {
-              id: user.id,
-            },
+    await ctx.prisma.session.create({
+      data: {
+        user: {
+          connect: {
+            id: user.id,
           },
-          token: crypto.randomUUID(),
-          expiresAt: date,
         },
-      })
-      .catch((error) => {
-        if (error.code === "P2002") {
-          throw new Error("Session already exists");
-        }
-      });
-    if (session) {
-      return {
-        response: `Successfully logged in as ${input.email}`,
-      };
-    }
+        token: crypto.randomUUID(),
+        expiresAt: date,
+      },
+    });
+
+    return {
+      response: `Successfully logged in as ${input.email}`,
+    };
   });
 
 export const signOutProcedure = baseProcedureBuilder
