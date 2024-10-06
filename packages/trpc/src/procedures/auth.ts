fecb750144a8050fa00fa9f6a6bf598410c72310
tracyyh <huang.tr@northeastern.edu>
2024-10-06 15:36:30 -0400

    edit sign up/in/out routes

--- packages/trpc/src/procedures/auth.ts
+++ packages/trpc/src/procedures/auth.ts
@@ -3,21 +3,92 @@ import { z } from "zod";
 import { baseProcedureBuilder } from "../internal/init";
 
 export const signUpProcedure = baseProcedureBuilder
-    .input(
-        z.object({
-            email: z.string(),
-            password: z.string(),
-        }),
-    )
-    .mutation(async ({ ctx, input }) => {
-        ctx.prisma.user.create({
-            data: {
-                email: input.email,
-                password: input.password,
-            },
-        });
+  .input(
+    z.object({
+      email: z.string(),
+      password: z.string(),
+    }),
+  )
+  .mutation(async ({ ctx, input }) => {
+    const user = await ctx.prisma.user
+      .create({
+        data: {
+          email: input.email,
+          password: input.password,
+        },
+      })
+      .catch((error) => {
+        if (error.code === "P2002") {
+          throw new Error("Email already exists");
+        }
+      });
 
-        return {
-            response: `Successfully logged in as ${input.email}`,
-        };
+    if (user) {
+      return {
+        response: `Successfully signed up and logged in as ${user.email}`,
+      };
+    }
+  });
+
+export const signInProcedure = baseProcedureBuilder
+  .input(
+    z.object({
+      email: z.string(),
+      password: z.string(),
+    }),
+  )
+  .mutation(async ({ ctx, input }) => {
+    const user = await ctx.prisma.user.findUnique({
+      where: {
+        email: input.email,
+      },
     });
+
+    if (user?.password !== input.password) {
+      throw new Error("Invalid credentials");
+    }
+
+    const date = new Date();
+    date.setDate(date.getDate() + 30);
+
+    const session = await ctx.prisma.session
+      .create({
+        data: {
+          user: {
+            connect: {
+              id: user.id,
+            },
+          },
+          token: crypto.randomUUID(),
+          expiresAt: date,
+        },
+      })
+      .catch((error) => {
+        if (error.code === "P2002") {
+          throw new Error("Session already exists");
+        }
+      });
+    if (session) {
+      return {
+        response: `Successfully logged in as ${input.email}`,
+      };
+    }
+  });
+
+export const signOutProcedure = baseProcedureBuilder
+  .input(
+    z.object({
+      token: z.string(),
+    }),
+  )
+  .mutation(async ({ ctx, input }) => {
+    await ctx.prisma.session.delete({
+      where: {
+        token: input.token,
+      },
+    });
+
+    return {
+      response: "Successfully logged out",
+    };
+  });
