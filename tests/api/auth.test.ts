342564a9847f8b87632a75a6993e079f7cbec6da
tracyyh <huang.tr@northeastern.edu>
2024-10-06 16:15:41 -0400

    fix signout test

--- tests/api/auth.test.ts
+++ tests/api/auth.test.ts
@@ -1,39 +1,38 @@
 import { expect, test } from "bun:test";
+
 import { _trpcCaller } from "@good-dog/trpc/server";
 
 test("auth/signIn", async () => {
-    const user = await _trpcCaller.signUp({
-        email: "damian@gmail.com",
-        password: "password",
-    });
+  const user = await _trpcCaller.signUp({
+    email: "damian@gmail.com",
+    password: "password",
+  });
 
-    const res = await _trpcCaller.signIn({
-        email: "damian@gmail.com",
-        password: "password",
-    });
+  const res = await _trpcCaller.signIn({
+    email: "damian@gmail.com",
+    password: "password",
+  });
 
-    const expectedRepsonse = {
-        message: "Successfully signed up and logged in as damian@gmail.com",
-        sessionToken: user.response.sessionToken,
-    };
+  const expectedRepsonse = {
+    message: "Successfully signed up and logged in as damian@gmail.com",
+    sessionToken: user.response.sessionToken,
+  };
 
-    expect(res.response).toEqual(expectedRepsonse);
+  expect(res.response).toEqual(expectedRepsonse);
 });
 
 test("auth/signOut", async () => {
-    const signInResponse = await _trpcCaller.signIn(
-        {
-            email: "test@email.com",
-            password: "test",
-        },
-    );
-    expect(signInResponse.response.message).toEqual("Successfully logged in as test@email.com");
+  const signInResponse = await _trpcCaller.signIn({
+    email: "test@email.com",
+    password: "test",
+  });
+  expect(signInResponse.response.message).toEqual(
+    "Successfully logged in as test@email.com",
+  );
 
-    const res = await _trpcCaller.signOut(
-        {
-            token: signInResponse.sessionToken,
-        },
-    );
+  const res = await _trpcCaller.signOut({
+    token: signInResponse.response.sessionToken ?? "",
+  });
 
-    expect(res.response).toEqual("Successfully logged out");
+  expect(res.response).toEqual("Successfully logged out");
 });
