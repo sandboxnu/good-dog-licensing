2ccad8458461fee922b5988d0ac605b58fe3a66d
tracyyh <huang.tr@northeastern.edu>
2024-10-16 19:35:12 -0400

    fix bcrypt issues

--- packages/trpc/src/procedures/auth.ts
+++ packages/trpc/src/procedures/auth.ts
@@ -1,161 +1,161 @@
+import { compare, genSalt, hash } from "bcryptjs";
 import { z } from "zod";
+
 import { baseProcedureBuilder } from "../internal/init";
-const bcrypt = require('bcryptjs');
 
 const hashPassword = async (password: string) => {
-    const saltRounds = 10; // Number of salt rounds (higher is more secure, but slower)
-    const salt = await bcrypt.genSalt(saltRounds);
-    const hash = await bcrypt.hash(password, salt);
-    return hash;
-}
+  const saltRounds = 10; // Number of salt rounds (higher is more secure, but slower)
+  const salt = await genSalt(saltRounds);
+  return await hash(password, salt);
+};
 
 export const signUpProcedure = baseProcedureBuilder
-    .input(
-        z.object({
-            email: z.string(),
-            password: z.string(),
-        }),
-    )
-    .mutation(async ({ ctx, input }) => {
-        const existingUser = await ctx.prisma.user.findUnique({
-            where: {
-                email: input.email,
-            },
-        });
-
-        if (existingUser) {
-            throw new Error(`User already exists for ${input.email}`);
-        }
-
-        const hashedPassword = await hashPassword(input.password);
-
-        const user = await ctx.prisma.user.create({
-            data: {
-                email: input.email,
-                password: hashedPassword,
-            },
-        });
-
-        const session = await ctx.prisma.session.create({
-            data: {
-                user: {
-                    connect: {
-                        id: user.id,
-                    },
-                },
-                token: crypto.randomUUID(),
-                expiresAt: new Date(),
-            },
-        });
-
-        return {
-            message: `Successfully signed up and logged in as ${input.email}`,
-            sessionToken: session.token,
-        };
+  .input(
+    z.object({
+      email: z.string(),
+      password: z.string(),
+    }),
+  )
+  .mutation(async ({ ctx, input }) => {
+    const existingUser = await ctx.prisma.user.findUnique({
+      where: {
+        email: input.email,
+      },
     });
+
+    if (existingUser) {
+      throw new Error(`User already exists for ${input.email}`);
+    }
+
+    const hashedPassword = await hashPassword(input.password);
+
+    const user = await ctx.prisma.user.create({
+      data: {
+        email: input.email,
+        password: hashedPassword,
+      },
+    });
+
+    const session = await ctx.prisma.session.create({
+      data: {
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+        token: crypto.randomUUID(),
+        expiresAt: new Date(),
+      },
+    });
+
+    return {
+      message: `Successfully signed up and logged in as ${input.email}`,
+      sessionToken: session.token,
+    };
+  });
 
 export const signInProcedure = baseProcedureBuilder
-    .input(
-        z.object({
-            email: z.string(),
-            password: z.string(),
-        }),
-    )
-    .mutation(async ({ ctx, input }) => {
-        const user = await ctx.prisma.user.findUnique({
-            where: {
-                email: input.email,
-            },
-        });
-
-        if (!user) {
-            throw new Error("Invalid credentials");
-        }
-
-        const match = await bcrypt.compare(input.password, user.password);
-
-        if (!match) {
-            throw new Error("Invalid credentials");
-        }
-
-        const date = new Date();
-        date.setDate(date.getDate() + 30);
-
-        await ctx.prisma.session.create({
-            data: {
-                user: {
-                    connect: {
-                        id: user.id,
-                    },
-                },
-                token: crypto.randomUUID(),
-                expiresAt: date,
-            },
-        });
-
-        const session = await ctx.prisma.session.findFirst({
-            where: {
-                userId: user.id,
-                expiresAt: {
-                    gte: new Date(),
-                },
-            },
-        });
-
-        return {
-            message: `Successfully logged in as ${input.email}`,
-            sessionToken: session?.token,
-        };
+  .input(
+    z.object({
+      email: z.string(),
+      password: z.string(),
+    }),
+  )
+  .mutation(async ({ ctx, input }) => {
+    const user = await ctx.prisma.user.findUnique({
+      where: {
+        email: input.email,
+      },
     });
+
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+
+    const match = await compare(input.password, user.password);
+
+    if (!match) {
+      throw new Error("Invalid credentials");
+    }
+
+    const date = new Date();
+    date.setDate(date.getDate() + 30);
+
+    await ctx.prisma.session.create({
+      data: {
+        user: {
+          connect: {
+            id: user.id,
+          },
+        },
+        token: crypto.randomUUID(),
+        expiresAt: date,
+      },
+    });
+
+    const session = await ctx.prisma.session.findFirst({
+      where: {
+        userId: user.id,
+        expiresAt: {
+          gte: new Date(),
+        },
+      },
+    });
+
+    return {
+      message: `Successfully logged in as ${input.email}`,
+      sessionToken: session?.token,
+    };
+  });
 
 export const signOutProcedure = baseProcedureBuilder
-    .input(
-        z.object({
-            token: z.string(),
-        }),
-    )
-    .mutation(async ({ ctx, input }) => {
-        await ctx.prisma.session.delete({
-            where: {
-                token: input.token,
-            },
-        });
-
-        return {
-            message: "Successfully logged out",
-        };
+  .input(
+    z.object({
+      token: z.string(),
+    }),
+  )
+  .mutation(async ({ ctx, input }) => {
+    await ctx.prisma.session.delete({
+      where: {
+        token: input.token,
+      },
     });
+
+    return {
+      message: "Successfully logged out",
+    };
+  });
 
 export const deleteAccountIfExistsProcedure = baseProcedureBuilder
-    .input(
-        z.object({
-            email: z.string(),
-        }),
-    )
-    .mutation(async ({ ctx, input }) => {
-        const user = await ctx.prisma.user.findUnique({
-            where: {
-                email: input.email,
-            },
-        });
-
-        if (!user) {
-            throw new Error(`No user found for ${input.email}`);
-        }
-
-        await ctx.prisma.session.deleteMany({
-            where: {
-                userId: user.id,
-            },
-        });
-
-        await ctx.prisma.user.delete({
-            where: {
-                email: input.email,
-            },
-        });
-
-        return {
-            message: "Successfully deleted account",
-        };
+  .input(
+    z.object({
+      email: z.string(),
+    }),
+  )
+  .mutation(async ({ ctx, input }) => {
+    const user = await ctx.prisma.user.findUnique({
+      where: {
+        email: input.email,
+      },
     });
+
+    if (!user) {
+      throw new Error(`No user found for ${input.email}`);
+    }
+
+    await ctx.prisma.session.deleteMany({
+      where: {
+        userId: user.id,
+      },
+    });
+
+    await ctx.prisma.user.delete({
+      where: {
+        email: input.email,
+      },
+    });
+
+    return {
+      message: "Successfully deleted account",
+    };
+  });
