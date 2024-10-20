8db5d1bde3517009fbdc001633cc2219f3f51c3e
tracyyh <125928988+tracyyh@users.noreply.github.com>
2024-10-20 14:35:14 -0400

    Create Post routes for sign up/in/out (#10)
    * sign in route created
    
    * sign in route template completed
    
    * edit sign up/in/out routes
    
    * sign up done
    
    * fix errors
    
    * changed responses for signup/in to include a message and a token
    
    * add signout test
    
    * fix signout test
    
    * add tests for auth
    
    * remove console.logs
    
    * bcrypt in progress
    
    * fix auth tests
    
    * revert db url
    
    * add shadow db for tests
    
    * fix bcrypt issues
    
    * change schema
    
    * maybe works
    
    * fix db url
    
    * try to fix tests
    
    * db push before tests
    
    * interactive false
    
    * fix vscode settings
    
    * add email validation
    
    ---------
    
    Co-authored-by: DamianUduevbo <58115973+DamianUduevbo@users.noreply.github.com>
    Co-authored-by: owen <owensimpson1127@gmail.com>

--- packages/trpc/src/internal/router.ts
+++ packages/trpc/src/internal/router.ts
@@ -1,8 +1,16 @@
-import { helloProcedure } from "../procedures/hello";
+import {
+  deleteAccountIfExistsProcedure,
+  signInProcedure,
+  signOutProcedure,
+  signUpProcedure,
+} from "../procedures/auth";
 import { createTRPCRouter } from "./init";
 
 export const appRouter = createTRPCRouter({
-  hello: helloProcedure,
+  signIn: signInProcedure,
+  signOut: signOutProcedure,
+  signUp: signUpProcedure,
+  deleteAccount: deleteAccountIfExistsProcedure,
 });
 
 export type AppRouter = typeof appRouter;
