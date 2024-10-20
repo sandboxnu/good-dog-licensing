9b740b40e7d1c8450716b34ec6b09c766fcaaa30
Sanjana Singhania <147664492+sanjana-singhania@users.noreply.github.com>
2024-10-20 15:03:02 -0400

    Added good-dog colors and changed component file structure (#12)
    * made new dir for shad components
    
    * added fix to remove css warnings
    
    * added new colors to fit dark scheme from violet onwards
    
    * fixed react import
    
    * updated lockfile

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -7,6 +7,6 @@ import type { Config } from "tailwindcss";
 import baseConfig from "@good-dog/tailwind/web";
 
 export default {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: ["./shad/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   presets: [baseConfig],
 } satisfies Config;
