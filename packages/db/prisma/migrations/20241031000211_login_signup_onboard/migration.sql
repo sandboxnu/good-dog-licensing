81a812a01da4b8039315e8e353984d4cb5ee1ad1
DamianUduevbo <58115973+DamianUduevbo@users.noreply.github.com>
2024-10-30 20:08:35 -0400

    mirgations

---
+++ packages/db/prisma/migrations/20241031000211_login_signup_onboard/migration.sql
@@ -0,0 +1,42 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - Added the required column `firstName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `lastName` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `role` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('MUSICIAN', 'MEDIA_MAKER', 'ADMIN');
+
+-- AlterTable
+ALTER TABLE "User"
+RENAME COLUMN "name" TO "firstName",
+ADD COLUMN     "lastName" TEXT NOT NULL,
+ADD COLUMN     "role" "Role" NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Group" (
+    "groupId" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Group_pkey" PRIMARY KEY ("groupId")
+);
+
+-- CreateTable
+CREATE TABLE "UserToGroup" (
+    "groupId" TEXT NOT NULL,
+    "email" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserToGroup_groupId_email_key" ON "UserToGroup"("groupId", "email");
+
+-- AddForeignKey
+ALTER TABLE "UserToGroup" ADD CONSTRAINT "UserToGroup_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("groupId") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserToGroup" ADD CONSTRAINT "UserToGroup_email_fkey" FOREIGN KEY ("email") REFERENCES "User"("email") ON DELETE RESTRICT ON UPDATE CASCADE;
