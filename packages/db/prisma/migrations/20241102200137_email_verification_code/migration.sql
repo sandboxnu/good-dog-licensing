97bb5f394ace00f86d6192470da0c7ae4dde133e
Jordan Praissman <jordanpraissman@gmail.com>
2024-11-02 16:01:59 -0400

    Created db migration

---
+++ packages/db/prisma/migrations/20241102200137_email_verification_code/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "EmailVerificationCode" (
+    "email" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "emailConfirmed" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "EmailVerificationCode_pkey" PRIMARY KEY ("email")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "EmailVerificationCode_email_key" ON "EmailVerificationCode"("email");
