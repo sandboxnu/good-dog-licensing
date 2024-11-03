193d73debd45ea53396904fb6fd103ceeed3c42a
owen <owensimpson1127@gmail.com>
2024-11-03 17:33:38 -0500

    create group schema

---
+++ packages/db/prisma/migrations/20241103222852_groups/migration.sql
@@ -0,0 +1,45 @@
+-- CreateTable
+CREATE TABLE "Group" (
+    "groupId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Group_pkey" PRIMARY KEY ("groupId")
+);
+
+-- CreateTable
+CREATE TABLE "GroupInvite" (
+    "groupId" TEXT NOT NULL,
+    "intitiatorId" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_GroupToUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "GroupInvite_groupId_email_key" ON "GroupInvite"("groupId", "email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_GroupToUser_AB_unique" ON "_GroupToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_GroupToUser_B_index" ON "_GroupToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "GroupInvite" ADD CONSTRAINT "GroupInvite_groupId_fkey" FOREIGN KEY ("groupId") REFERENCES "Group"("groupId") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "GroupInvite" ADD CONSTRAINT "GroupInvite_intitiatorId_fkey" FOREIGN KEY ("intitiatorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_GroupToUser" ADD CONSTRAINT "_GroupToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Group"("groupId") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_GroupToUser" ADD CONSTRAINT "_GroupToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
