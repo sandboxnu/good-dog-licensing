0e7969450cf46dc8e3cb758ea2b01a3d0a3ba9d0
owen <owensimpson1127@gmail.com>
2024-11-11 11:52:28 -0500

    test

--- apps/web/app/(registration)/onboarding/page.tsx
+++ apps/web/app/(registration)/onboarding/page.tsx
@@ -1,6 +1,9 @@
 import { OnboardingForm } from "@good-dog/components/registration";
 import { trpc } from "@good-dog/trpc/server";
 
+// Necessary to access cookies
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   const user = await trpc.user();
 
