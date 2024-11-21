3ef2c0953cb8ca695aa43209a56ffde504b04361
owen <owensimpson1127@gmail.com>
2024-11-20 20:03:42 -0500

    test

--- apps/web/app/(pages)/layout.tsx
+++ apps/web/app/(pages)/layout.tsx
@@ -1,17 +1,18 @@
 import Footer from "@good-dog/components/Footer";
 import Nav from "@good-dog/components/Nav";
-import { HydrateClient, trpc } from "@good-dog/trpc/server";
+
+// import { HydrateClient, trpc } from "@good-dog/trpc/server";
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  void trpc.user.prefetch();
+  // void trpc.user.prefetch();
 
   return (
-    <HydrateClient>
+    <>
       <Nav />
       {children}
       <Footer />
-    </HydrateClient>
+    </>
   );
 }
