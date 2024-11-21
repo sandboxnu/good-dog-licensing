d3dded00dc7fcc2ef0c267fb1752e20d490c39c5
Jordan Praissman <jordanpraissman@gmail.com>
2024-11-20 19:42:23 -0500

    finished forgot-password procedures

--- packages/trpc/src/procedures/forgot-password.ts
+++ packages/trpc/src/procedures/forgot-password.ts
@@ -1,30 +1,31 @@
-import { setSessionCookie } from "@good-dog/auth/cookies";
-import { hashPassword } from "@good-dog/auth/password";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
-import { notAuthenticatedProcedureBuilder } from "../internal/init";
-import { generateSixDigitCode } from "@good-dog/email/email-service";
+
+import { hashPassword } from "@good-dog/auth/password";
 import { sendPasswordResetEmail } from "@good-dog/email/password-reset-email";
 
-// click forgot passwd -> enter email -> receive email with code -> enter code & new passwd
+import { baseProcedureBuilder } from "../internal/init";
 
-const getNewEmailVerificationCodeExpirationDate = () =>
+// click forgot passwd -> enter email -> receive unique link -> enter new passwd at given link
+
+const getNewPasswordResetExpirationDate = () =>
   new Date(Date.now() + 60_000 * 15);
 
-export const sendForgotPasswordEmailProcedure = notAuthenticatedProcedureBuilder
+export const sendForgotPasswordEmailProcedure = baseProcedureBuilder
   .input(
     z.object({
       email: z.string().email(),
-      newPassword: z.string(),
     }),
   )
   .mutation(async ({ ctx, input }) => {
+    // Find user with the given email
     const user = await ctx.prisma.user.findUnique({
       where: {
         email: input.email,
       },
     });
 
+    // If user with that email doesn't exist, throw error
     if (!user) {
       throw new TRPCError({
         code: "NOT_FOUND",
@@ -32,97 +33,107 @@ export const sendForgotPasswordEmailProcedure = notAuthenticatedProcedureBuilder
       });
     }
 
-    // create reset reques. if it exists then delete then create a new one
+    // Delete any existing password reset requests for the user
+    await ctx.prisma.passwordResetReq.deleteMany({
+      where: {
+        user: user,
+      },
+    });
+
+    // Create new password reset request with updated expired at time
     const pwdResetReq = await ctx.prisma.passwordResetReq.create({
       data: {
         user: {
           connect: {
             userId: user.userId,
-          }
+          },
         },
-        expiresAt: getNewEmailVerificationCodeExpirationDate(),
+        expiresAt: getNewPasswordResetExpirationDate(),
       },
     });
 
-    try {
-      //await sendEmailVerification(input.email, emailCode);
-      await sendPasswordResetEmail(input.email,);
-    } catch (error) {
-      throw new TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: `Email confirmation to ${input.email} failed to send.`,
-        cause: error,
-      });
-    }
-
-    // Create/update the password reset code in the database
-    await ctx.prisma.passwordResetCode.upsert({
-      where: {
-        email: input.email,
-      },
-      update: {
-        code: emailCode,
-        newHashedPassword: await hashPassword(input.newPassword),
-        expiresAt: getNewEmailVerificationCodeExpirationDate(),
-      },
-      create: {
-        code: emailCode,
-        email: input.email,
-        newHashedPassword: await hashPassword(input.newPassword),
-        expiresAt: getNewEmailVerificationCodeExpirationDate(),
-      },
-    });
-
-    return {
-      message: `Password reset code sent to ${input.email}`,
-    };
-  });
-
-// when the user clicks the link in the email, they are taken to a page where they can enter the code and new password.
-export const confirmedPasswordResetProcedure = notAuthenticatedProcedureBuilder
-  .input(
-    z.object({
-      email: z.string().email(),
-      code: z.string(),
-    }),
-  )
-  .mutation(async ({ ctx, input }) => {
-    const passwordResetCode = await ctx.prisma.passwordResetCode.findUnique({
-      where: {
-        email: input.email,
-      },
-    });
-
-    if (!passwordResetCode
-      || passwordResetCode.code !== input.code
-      || passwordResetCode.expiresAt < new Date()) {
-      throw new TRPCError({
-        code: "UNAUTHORIZED",
-        message: `Invalid or expired code for email ${input.email}`,
-      });
-    }
-
-    // clear seassions and update their password
+    // Update the user to include the new password reset request
     await ctx.prisma.user.update({
       where: {
         email: input.email,
       },
       data: {
-        hashedPassword: passwordResetCode.newHashedPassword,
-        sessions: {
-          deleteMany: {},
+        passwordResetReq: {
+          connect: {
+            passwordResetId: pwdResetReq.passwordResetId,
+          },
         },
       },
     });
 
-    // Delete the password reset code
-    await ctx.prisma.passwordResetCode.delete({
+    // Send the password reset email
+    try {
+      await sendPasswordResetEmail(input.email, pwdResetReq.passwordResetId);
+    } catch (error) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: `Password reset email to ${input.email} failed to send.`,
+        cause: error,
+      });
+    }
+
+    return {
+      message: `Password reset email sent to ${input.email}.`,
+    };
+  });
+
+export const confirmePasswordResetProcedure = baseProcedureBuilder
+  .input(
+    z.object({
+      email: z.string().email(),
+      newPassword: z.string(),
+    }),
+  )
+  .mutation(async ({ ctx, input }) => {
+    // Find user with the given email
+    const user = await ctx.prisma.user.findUnique({
       where: {
         email: input.email,
+      },
+      include: {
+        passwordResetReq: true,
+      },
+    });
+
+    // If user with that email doesn't exist or they don't have a password reset request, throw error
+    if (!user?.passwordResetReq) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: `No password reset request for the given email.`,
+      });
+    }
+
+    // If password reset request is expired, throw error
+    if (user.passwordResetReq.expiresAt < new Date()) {
+      throw new TRPCError({
+        code: "UNAUTHORIZED",
+        message: `Password reset request is expired.`,
+      });
+    }
+
+    // Update user's password
+    await ctx.prisma.user.update({
+      where: {
+        email: input.email,
+      },
+      data: {
+        hashedPassword: await hashPassword(input.newPassword),
+      },
+    });
+
+    // Delete the password reset request
+    await ctx.prisma.passwordResetReq.deleteMany({
+      where: {
+        user: user,
       },
     });
 
     return {
-      message: "Code verified",
+      message: "Password reset.",
     };
   });
