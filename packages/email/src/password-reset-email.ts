d1fea1a20533e14439fd35cb0ceb11c94ba0a6a1
Jordan Praissman <jordanpraissman@gmail.com>
2024-11-24 14:58:03 -0500

    Correct baseURL

--- packages/email/src/password-reset-email.ts
+++ packages/email/src/password-reset-email.ts
@@ -4,10 +4,15 @@ import { env } from "@good-dog/env";
 export async function sendPasswordResetEmail(toEmail: string, cuid: string) {
   setApiKey(env.SENDGRID_API_KEY ?? "");
 
+  let baseURL = "http://localhost:3000";
+  if (env.VERCEL_URL) {
+    baseURL = `https://${env.VERCEL_URL}`;
+  }
+
   const msg = {
     to: toEmail,
     subject: "Reset Your Password - Good Dog Licensing",
-    html: `<p>Follow the link to reset your password: <strong>http://localhost:3000/pwdreset/reset_id?=${cuid}</strong></p>`,
+    html: `<p>Follow the link to reset your password: <strong>${baseURL}/pwdreset/reset_id?=${cuid}</strong></p>`,
   };
 
   return await send(msg, env.VERIFICATION_FROM_EMAIL ?? "");
