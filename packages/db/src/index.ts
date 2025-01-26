0b1f42628a6b9e8c866ea1dcd798e64cce486013
sanjana-singhania <singhania.sa@northeastern.edu>
2025-01-26 17:05:22 -0500

    fix

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,14 +1,7 @@
-import type { ReferralSource as PrismaReferralSource } from "@prisma/client";
+import type { ReferralSource } from "@prisma/client";
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
 
-export const ReferralSource: Record<PrismaReferralSource, string> = {
-  FRIEND: "Friend",
-  COLLEAGUE: "Colleague",
-  GREEN_LINE_RECORDS: "Green Line Records",
-  SOCIAL_MEDIA: "Social Media",
-  OTHER: "Other",
-} as const;
-
+export { ReferralSource };
 export type ReferralType = keyof typeof ReferralSource;
