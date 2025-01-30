adbbf35fb92998e0a273fe0138f55943e8b59430
owen <owensimpson1127@gmail.com>
2025-01-30 12:20:38 -0500

    env fix

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
+import { env } from "@good-dog/env";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("@good-dog/env");
+env;
 
 const config: NextConfig = {};
 
