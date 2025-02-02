0a76aa7f126ac375a003302f7e680ed172d5167b
Anzhuo-W <45926251+Anzhuo-W@users.noreply.github.com>
2025-02-02 16:47:05 -0500

    update migration

--- packages/db/prisma/migrations/20250202214637_add_phone_number/migration.sql
+++ packages/db/prisma/migrations/20250202214637_add_phone_number/migration.sql
@@ -6,3 +6,9 @@
 */
 -- AlterTable
 ALTER TABLE "User" ADD COLUMN     "phoneNumber" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "_songWriters" ADD CONSTRAINT "_songWriters_AB_pkey" PRIMARY KEY ("A", "B");
+
+-- DropIndex
+DROP INDEX "_songWriters_AB_unique";
