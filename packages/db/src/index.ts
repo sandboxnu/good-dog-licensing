b0ccd222609aa77a5cd241c0608ce21b10ca6982
owen <owensimpson1127@gmail.com>
2025-02-09 14:36:18 -0500

    fixes

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,4 +2,5 @@ import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient();
 
+// Re-export prisma types and enums here if needed for other packages
 export { Role } from "@prisma/client";
