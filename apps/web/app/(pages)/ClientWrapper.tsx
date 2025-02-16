bef3e02cd4b4ee0e0c50e0578269e318532c8c3f
owen <owensimpson1127@gmail.com>
2025-02-16 15:34:10 -0500

    maybe 2

--- apps/web/app/(pages)/ClientWrapper.tsx
+++ apps/web/app/(pages)/ClientWrapper.tsx
@@ -17,32 +17,3 @@ export const ClientWrapper = (
 
   return <>{props.children}</>;
 };
-
-/**
- * Due to limitations in the Next.js rendering engine, we can't set cookies during page load.
- *
- * That means we need shouldn't set any cookies in tRPC *queries*, and instead should use *mutations*
- * which will never be run at render time.
- *
- * This component will automatically refresh the session if the session is
- * marked as `refreshRequired`, as determined by the server.
- */
-const useSessionRefresh = () => {
-  const [user] = trpc.user.useSuspenseQuery();
-  const tUtils = trpc.useUtils();
-  const refreshSessionMutation = trpc.refreshSession.useMutation({
-    onSuccess: (response) => {
-      // Override the session with the new session data *if* there is already existing session data
-      tUtils.user.setData(undefined, (old) => (old ? response.user : old));
-      tUtils.authenticatedUser.setData(undefined, (old) =>
-        old ? response.user : old,
-      );
-    },
-  });
-
-  useEffect(() => {
-    if (user?.session.refreshRequired && !refreshSessionMutation.isPending) {
-      refreshSessionMutation.mutate();
-    }
-  }, [user?.session, refreshSessionMutation]);
-};
