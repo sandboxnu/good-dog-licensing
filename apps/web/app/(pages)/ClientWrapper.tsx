5d831b518cd0c3e5889d1b798e78edddd7318476
owen <owensimpson1127@gmail.com>
2025-02-16 15:35:13 -0500

    maybe 3

--- apps/web/app/(pages)/ClientWrapper.tsx
+++ apps/web/app/(pages)/ClientWrapper.tsx
@@ -1,9 +1,5 @@
 "use client";
 
-import { useEffect } from "react";
-
-import { trpc } from "@good-dog/trpc/client";
-
 /**
  * This component is used to wrap the entire application with any client-side
  * hooks or components that need to be run on every page.
@@ -13,7 +9,5 @@ export const ClientWrapper = (
     children: React.ReactNode;
   }>,
 ) => {
-  // useSessionRefresh();
-
   return <>{props.children}</>;
 };
