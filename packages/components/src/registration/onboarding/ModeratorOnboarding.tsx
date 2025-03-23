4270ca91888f7096a12744dab4df2c9e8c20a124
Jordan Praissman <jordanpraissman@gmail.com>
2025-03-23 19:12:08 -0400

    Lots of progess on design

--- packages/components/src/registration/onboarding/ModeratorOnboarding.tsx
+++ packages/components/src/registration/onboarding/ModeratorOnboarding.tsx
@@ -8,15 +8,15 @@ import { z } from "zod";
 import { trpc } from "@good-dog/trpc/client";
 
 const schema = z.object({
-  firstName: z.string(),
-  lastName: z.string(),
+  firstName: z.string().min(1, "First name is required"),
+  lastName: z.string().min(1, "Last name is required"),
   phoneNumber: z
     .string()
     .regex(
       /[-.\s]?(\(?\d{3}\)?)[-.\s]?\d{3}[-.\s]?\d{4}$/,
       "Phone number must be a valid US format such as 1234567890, 123-456-7890, or (123) 456-7890.",
     ),
-  password: z.string(),
+  password: z.string().min(1, "Password is required"),
 });
 
 type FormFields = z.infer<typeof schema>;
@@ -52,23 +52,122 @@ export default function ModeratorOnboarding() {
   });
 
   return (
-    <form
-      onSubmit={handleSubmit((data) => {
-        onboardModeratorMutation.mutate({
-          moderatorInviteId: moderatorInviteId,
-          ...data,
-        });
-      })}
-    >
-      <input {...register("firstName")} placeholder="First Name"></input>
-      <p>{errors.firstName?.message}</p>
-      <input {...register("lastName")} placeholder="Last Name"></input>
-      <p>{errors.lastName?.message}</p>
-      <input {...register("phoneNumber")} placeholder="Phone Number"></input>
-      <p>{errors.phoneNumber?.message}</p>
-      <input {...register("password")} placeholder="Password"></input>
-      <p>{errors.password?.message}</p>
-      <button type="submit">Create Account</button>
-    </form>
+    <div className="flex">
+      <div className="w-1/2 pl-20 pr-40 pt-28">
+        <h2 className="font-afacad text-5xl font-medium font-normal text-black">
+          Create your account
+        </h2>
+        <h3 className="font-afacad text-2xl font-medium font-normal text-black">
+          Welcome! Please enter your details.
+        </h3>
+        <form
+          className="pt-8"
+          onSubmit={handleSubmit((data) => {
+            onboardModeratorMutation.mutate({
+              moderatorInviteId: moderatorInviteId,
+              ...data,
+            });
+          })}
+        >
+          <div className="flex flex-col pb-6">
+            <div className="pb-2">
+              <label
+                className="font-afacad text-2xl font-medium font-normal text-black"
+                htmlFor="first-name"
+              >
+                First Name
+              </label>
+              <label className="font-afacad text-2xl font-normal font-semibold text-[#F4392D]">
+                {" *"}
+              </label>
+            </div>
+            <input
+              className="h-[40px] bg-[#D9D9D9] pl-[8px] placeholder-[#5F5F5F] placeholder:text-base placeholder:font-medium"
+              id="first-name"
+              {...register("firstName")}
+              placeholder="Enter your first name"
+            ></input>
+            <p className="font-afacad text-sm font-medium text-[#75747A]">
+              {errors.firstName?.message}
+            </p>
+          </div>
+          <div className="flex flex-col pb-6">
+            <div className="pb-2">
+              <label
+                className="font-afacad text-2xl font-medium font-normal text-black"
+                htmlFor="last-name"
+              >
+                Last Name
+              </label>
+              <label className="font-afacad text-2xl font-normal font-semibold text-[#F4392D]">
+                {" *"}
+              </label>
+            </div>
+            <input
+              className="h-[40px] bg-[#D9D9D9] pl-[8px] placeholder-[#5F5F5F] placeholder:text-base placeholder:font-medium"
+              id="last-name"
+              {...register("lastName")}
+              placeholder="Enter your last name"
+            ></input>
+            <p className="font-afacad text-sm font-medium text-[#75747A]">
+              {errors.lastName?.message}
+            </p>
+          </div>
+          <div className="flex flex-col pb-6">
+            <div className="pb-2">
+              <label
+                className="font-afacad text-2xl font-medium font-normal text-black"
+                htmlFor="phone-number"
+              >
+                Phone Number
+              </label>
+              <label className="font-afacad text-2xl font-normal font-semibold text-[#F4392D]">
+                {" *"}
+              </label>
+            </div>
+            <input
+              className="h-[40px] bg-[#D9D9D9] pl-[8px] placeholder-[#5F5F5F] placeholder:text-base placeholder:font-medium"
+              id="phone-number"
+              {...register("phoneNumber")}
+              placeholder="Enter your phone number"
+            ></input>
+            <p className="font-afacad text-sm font-medium text-[#75747A]">
+              {errors.phoneNumber?.message}
+            </p>
+          </div>
+          <div className="flex flex-col pb-6">
+            <div className="pb-2">
+              <label
+                className="font-afacad text-2xl font-medium font-normal text-black"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <label className="font-afacad text-2xl font-normal font-semibold text-[#F4392D]">
+                {" *"}
+              </label>
+            </div>
+            <input
+              className="h-[40px] bg-[#D9D9D9] pl-[8px] placeholder-[#5F5F5F] placeholder:text-base placeholder:font-medium"
+              id="password"
+              {...register("password")}
+              placeholder="Enter your password"
+            ></input>
+            <p className="font-afacad text-sm font-medium text-[#75747A]">
+              {errors.password?.message}
+            </p>
+          </div>
+          <div className="flex items-center justify-center pt-4">
+            <button
+              className="font-afacad h-[59px] w-3/4 rounded-2xl bg-black text-2xl font-medium text-white"
+              type="submit"
+            >
+              Create Account
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="w-1/2 bg-red-100">Test</div>
+    </div>
   );
 }
