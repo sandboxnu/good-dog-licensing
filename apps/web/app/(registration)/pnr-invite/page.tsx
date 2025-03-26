97c88265be9a71574e7b100b41c80d96599e2b23
Jordan Praissman <jordanpraissman@gmail.com>
2025-03-25 23:59:36 -0400

    Maybe fixed

--- apps/web/app/(registration)/pnr-invite/page.tsx
+++ apps/web/app/(registration)/pnr-invite/page.tsx
@@ -1,10 +1,12 @@
 import ModeratorOnboarding from "@good-dog/components/registration/onboarding/ModeratorOnboarding";
 
-export default function Page({
-  searchParams,
-}: {
-  searchParams: { id?: string };
-}) {
+interface PageProps {
+  searchParams: {
+    id?: string;
+  };
+}
+
+export default function Page({ searchParams }: PageProps) {
   const moderatorInviteId = searchParams.id ?? "";
 
   return <ModeratorOnboarding moderatorInviteId={moderatorInviteId} />;
