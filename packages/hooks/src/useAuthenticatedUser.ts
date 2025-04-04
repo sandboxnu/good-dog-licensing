a97443124b6c740a50fb3ceaada908b389d27fb7
owen <owensimpson1127@gmail.com>
2025-04-03 20:00:49 -0400

    update doc

--- packages/hooks/src/useAuthenticatedUser.ts
+++ packages/hooks/src/useAuthenticatedUser.ts
@@ -4,6 +4,16 @@ import type { TRPCHookResult } from "@trpc/react-query/shared";
 import type { UserWithSession } from "@good-dog/trpc/types";
 import { trpc } from "@good-dog/trpc/client";
 
+/**
+ * This hook is used to get the authenticated user from the server.
+ * It should throw an error if the user is not authenticated.
+ *
+ * This hook is meant to be used inside a restricted route, and under normal
+ * circumstances the error should never be thrown.
+ *
+ * Use the standard `trpc.user.useQuery` hook if you want to allow non-authenticated
+ * users to access the route.
+ */
 export const useAuthenticatedUserSuspense = (
   onUnauthenticated: () => never,
   ...args: Parameters<typeof trpc.user.useSuspenseQuery<UserWithSession | null>>
