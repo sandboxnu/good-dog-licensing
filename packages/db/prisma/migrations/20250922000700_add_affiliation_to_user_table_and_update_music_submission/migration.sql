3c241381247745b0f133b3e546110a9ac0dabaef
Jordan Praissman <jordanpraissman@gmail.com>
2025-09-21 20:08:32 -0400

    Update db:

--- packages/db/prisma/migrations/20250922000700_add_affiliation_to_user_table_and_update_music_submission/migration.sql
+++ packages/db/prisma/migrations/20250922000700_add_affiliation_to_user_table_and_update_music_submission/migration.sql
@@ -1,16 +1,22 @@
 /*
   Warnings:
 
+  - You are about to drop the column `affliation` on the `MusicContributor` table. All the data in the column will be lost.
   - You are about to drop the column `artistId` on the `MusicSubmission` table. All the data in the column will be lost.
   - You are about to drop the column `isAscapAffiliated` on the `User` table. All the data in the column will be lost.
   - You are about to drop the column `isBmiAffiliated` on the `User` table. All the data in the column will be lost.
   - You are about to drop the column `isSongWriter` on the `User` table. All the data in the column will be lost.
+  - Added the required column `affiliation` to the `MusicContributor` table without a default value. This is not possible if the table is not empty.
   - Added the required column `performerName` to the `MusicSubmission` table without a default value. This is not possible if the table is not empty.
   - Added the required column `submitterId` to the `MusicSubmission` table without a default value. This is not possible if the table is not empty.
 
 */
 -- DropForeignKey
 ALTER TABLE "MusicSubmission" DROP CONSTRAINT "MusicSubmission_artistId_fkey";
+
+-- AlterTable
+ALTER TABLE "MusicContributor" DROP COLUMN "affliation",
+ADD COLUMN     "affiliation" "MusicAffiliation" NOT NULL;
 
 -- AlterTable
 ALTER TABLE "MusicSubmission" DROP COLUMN "artistId",
@@ -21,7 +27,7 @@ ADD COLUMN     "submitterId" TEXT NOT NULL;
 ALTER TABLE "User" DROP COLUMN "isAscapAffiliated",
 DROP COLUMN "isBmiAffiliated",
 DROP COLUMN "isSongWriter",
-ADD COLUMN     "affliation" "MusicAffiliation",
+ADD COLUMN     "affiliation" "MusicAffiliation",
 ADD COLUMN     "ipi" TEXT;
 
 -- AddForeignKey
