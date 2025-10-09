96a4c266371fb3d76e62c1f6f96608e5f2abcdb3
Lauren Brissette <168485735+laurenbrissette@users.noreply.github.com>
2025-10-09 19:28:19 -0400

    fix tests

---
+++ tests/api/matches.test.ts
@@ -0,0 +1,244 @@
+import {
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  expect,
+  it,
+} from "bun:test";
+
+import { passwordService } from "@good-dog/auth/password";
+import { prisma } from "@good-dog/db";
+import { $createTrpcCaller } from "@good-dog/trpc/server";
+
+import { MockNextCache } from "../mocks/MockNextCache";
+import { MockNextCookies } from "../mocks/MockNextCookies";
+import { createMockCookieService } from "../mocks/util";
+
+async function createData() {
+  await prisma.user.create({
+    data: {
+      userId: "matcher",
+      firstName: "Jane",
+      lastName: "Doe",
+      role: "MODERATOR",
+      phoneNumber: "1234556789",
+      email: "bestmod123@gmail.com",
+      hashedPassword: await passwordService.hashPassword("passcodeispasscode"),
+      sessions: {
+        create: {
+          sessionId: "moderator-session-id",
+          expiresAt: new Date(Date.now() + 5_000_000_000),
+        },
+      },
+    },
+  });
+
+  const musician = await prisma.user.create({
+    data: {
+      userId: "musician",
+      firstName: "ABCDEF",
+      lastName: "ABCDEF",
+      role: "MUSICIAN",
+      phoneNumber: "2234567890",
+      email: "musician@gmail.com",
+      hashedPassword: await passwordService.hashPassword("password123"),
+      sessions: {
+        create: {
+          sessionId: "musician-session-id",
+          expiresAt: new Date(Date.now() + 5_000_000_000),
+        },
+      },
+    },
+  });
+
+  await prisma.musicSubmission.create({
+    data: {
+      musicId: "musicSubmission",
+      songName: "3005",
+      songLink:
+        "https://open.spotify.com/track/2X6b7zLdIxCejd6GqVcQ9M?si=b36f9306fab04109",
+      genre: "hip hop",
+      submitterId: musician.userId,
+      performerName: "The Beatles",
+    },
+  });
+
+  const projectOwner = await prisma.user.create({
+    data: {
+      userId: "projectOwner",
+      firstName: "Project",
+      lastName: "Owner",
+      role: "MEDIA_MAKER",
+      phoneNumber: "3234567890",
+      email: "project@gmail.com",
+      hashedPassword: await passwordService.hashPassword("password123"),
+      sessions: {
+        create: {
+          sessionId: "project-session-id",
+          expiresAt: new Date(Date.now() + 5_000_000_000),
+        },
+      },
+    },
+  });
+
+  const projectSubmission = await prisma.projectSubmission.create({
+    data: {
+      projectId: "projectSubmission",
+      projectOwnerId: projectOwner.userId,
+      projectTitle: "title",
+      description: "a project hoping to showcase the effects of climate change",
+      deadline: new Date(Date.now() + 2_000_000_000),
+    },
+  });
+
+  await prisma.songRequest.create({
+    data: {
+      oneLineSummary: "Test One Line Summary",
+      songRequestId: "songRequestOneSubmission",
+      description: "wildfires in CA",
+      musicType: "rnb, soul",
+      projectId: projectSubmission.projectId,
+    },
+  });
+
+  await prisma.user.create({
+    data: {
+      userId: "sanjana",
+      firstName: "ABCDEF",
+      lastName: "ABCDEF",
+      role: "ADMIN",
+      phoneNumber: "4234567890",
+      email: "sanjana@gmail.com",
+      hashedPassword: await passwordService.hashPassword("password123"),
+      sessions: {
+        create: {
+          sessionId: "sanjana-session-id",
+          expiresAt: new Date(Date.now() + 5_000_000_000),
+        },
+      },
+    },
+  });
+}
+
+async function deleteData() {
+  // Delete Comments (created in tests)
+  await prisma.comments.deleteMany({
+    where: {
+      songRequestId: "songRequestOneSubmission",
+    },
+  });
+
+  // Delete Match
+  await prisma.match.deleteMany({
+    where: { matchId: "match" },
+  });
+
+  // Delete Match for suggestMatch tests
+  await prisma.match.deleteMany({
+    where: {
+      songRequestId: "songRequestOneSubmission",
+    },
+  });
+
+  // Delete SongRequest
+  await prisma.songRequest.deleteMany({
+    where: { songRequestId: "songRequestOneSubmission" },
+  });
+
+  // Delete ProjectSubmission
+  await prisma.projectSubmission.deleteMany({
+    where: { projectId: "projectSubmission" },
+  });
+
+  // Delete MusicSubmission
+  await prisma.musicSubmission.deleteMany({
+    where: { musicId: "musicSubmission" },
+  });
+
+  // Delete Users
+  await prisma.user.deleteMany({
+    where: {
+      userId: {
+        in: ["matcher", "musician", "projectOwner", "sanjana"],
+      },
+    },
+  });
+}
+
+describe("match procedure", () => {
+  const cookies = new MockNextCookies();
+  const cache = new MockNextCache();
+
+  beforeAll(async () => {
+    await cache.apply();
+  });
+
+  beforeEach(async () => {
+    await createData();
+  });
+
+  const $api = $createTrpcCaller({
+    cookiesService: createMockCookieService(cookies),
+    prisma: prisma,
+  });
+
+  afterEach(async () => {
+    await deleteData();
+    cookies.clear();
+    cache.clear();
+  });
+
+  it("should allow a moderator to create a match", async () => {
+    cookies.set("sessionId", "moderator-session-id");
+
+    const response = await $api.createMatch({
+      songRequestId: "songRequestOneSubmission",
+      musicId: "musicSubmission",
+    });
+
+    expect(response.message).toEqual("Match successfully created.");
+
+    const match = await prisma.match.findFirst({
+      where: {
+        songRequestId: "songRequestOneSubmission",
+        musicId: "musicSubmission",
+        matcherUserId: "matcher",
+      },
+    });
+
+    expect(match).toBeDefined();
+  });
+
+  it("should allow an admin to create a match", async () => {
+    cookies.set("sessionId", "sanjana-session-id");
+
+    const response = await $api.createMatch({
+      songRequestId: "songRequestOneSubmission",
+      musicId: "musicSubmission",
+    });
+
+    expect(response.message).toEqual("Match successfully created.");
+
+    const match = await prisma.match.findFirst({
+      where: {
+        songRequestId: "songRequestOneSubmission",
+        musicId: "musicSubmission",
+        matcherUserId: "sanjana",
+      },
+    });
+
+    expect(match).toBeDefined();
+  });
+
+  it("should not allow a normal user to create a match", () => {
+    cookies.set("sessionId", "musician-session-id");
+
+    expect(
+      $api.createMatch({
+        songRequestId: "songRequestOneSubmission",
+        musicId: "musicSubmission",
+      }),
+    ).rejects.toThrow("permission to modify");
+  });
+});
