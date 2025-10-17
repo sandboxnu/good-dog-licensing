ecd08d45b759a099b96e8a6ac5dcf6c4aa19f3a9
WesleyTran0 <wesley.tran24@gmail.com>
2025-10-17 18:07:27 -0400

    got the page to look just like the designs. Not sure if it is fully implemented; will test that in second

--- apps/web/app/(registration)/login/page.tsx
+++ apps/web/app/(registration)/login/page.tsx
@@ -1,6 +1,11 @@
 import RegistrationPageLayout from "@good-dog/components/oldStuff/registration/RegistrationPageLayout";
-import SignInForm from "@good-dog/components/oldStuff/registration/SignInForm";
+import LoginWidget from "@good-dog/components/sign-up/login-widget/LoginWidget";
+// import SignInForm from "@good-dog/components/oldStuff/registration/SignInForm";
 
 export default function Page() {
-  return <RegistrationPageLayout form={<SignInForm />} formLocation="right" />;
+  return (
+    <div className="p-[100px]">
+      <LoginWidget />
+
+    </div>)
 }
