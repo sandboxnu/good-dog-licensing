73d274df681c81bb1dd2318f31a5c39d510c0f6d
Lauren Brissette <168485735+laurenbrissette@users.noreply.github.com>
2025-11-01 10:55:15 -0400

    Flex global and svg style updates (#267)
    * h3 styling
    
    * fix hydration error
    
    * add images to the homepage
    
    * icons

--- packages/components/src/user-onboarding/widgets/sign-up-widget/EmailCodeModal.tsx
+++ packages/components/src/user-onboarding/widgets/sign-up-widget/EmailCodeModal.tsx
@@ -36,52 +36,52 @@ export default function EmailCodeModal({
       }}
     >
       <DialogOverlay className="!bg-transparent" />
-      <DialogContent className="w-full bg-white !rounded-[16px] border border-solid border-black">
+      <DialogContent className="w-full bg-white !rounded-[16px] border border-solid border-black justify-center">
         <DialogHeader className="!text-center pt-[24px]">
           <DialogTitle className="!text-h3 font-medium">
             Verify your email address
           </DialogTitle>
-          <DialogDescription className="flex flex-col items-center gap-[16px]">
-            <p className="!text-body2">{`We sent a link to ${email}`}</p>
-            <InputOTP
-              maxLength={6}
-              value={emailCode}
-              onChange={(newCode) => {
-                setEmailCode(newCode);
-                if (newCode.length == 6) {
-                  verifyCode(newCode);
-                }
-              }}
-            >
-              <InputOTPGroup className="flex flex-row gap-[16px]">
-                <InputOTPSlot
-                  index={0}
-                  className="text-h1 h-[64px] w-[48px] border border-solid border-[#858585] rounded-[4px]"
-                />
-                <InputOTPSlot
-                  index={1}
-                  className="text-h1 h-[64px] w-[48px] border border-solid border-[#858585] rounded-[4px]"
-                />
-                <InputOTPSlot
-                  index={2}
-                  className="text-h1 h-[64px] w-[48px] border border-solid border-[#858585] rounded-[4px]"
-                />
-                <InputOTPSlot
-                  index={3}
-                  className="text-h1 h-[64px] w-[48px] border border-solid border-[#858585] rounded-[4px]"
-                />
-                <InputOTPSlot
-                  index={4}
-                  className="text-h1 h-[64px] w-[48px] border border-solid border-[#858585] rounded-[4px]"
-                />
-                <InputOTPSlot
-                  index={5}
-                  className="text-h1 h-[64px] w-[48px] border border-solid border-[#858585] rounded-[4px]"
-                />
-              </InputOTPGroup>
-            </InputOTP>
+          <DialogDescription className="flex flex-col items-center gap-[16px] !text-body2">
+            We sent a link to {email}
           </DialogDescription>
         </DialogHeader>
+        <InputOTP
+          maxLength={6}
+          value={emailCode}
+          onChange={(newCode) => {
+            setEmailCode(newCode);
+            if (newCode.length == 6) {
+              verifyCode(newCode);
+            }
+          }}
+        >
+          <InputOTPGroup className="flex flex-row gap-[16px]">
+            <InputOTPSlot
+              index={0}
+              className="text-h1 h-[64px] w-[48px] border border-solid border-[#858585] rounded-[4px]"
+            />
+            <InputOTPSlot
+              index={1}
+              className="text-h1 h-[64px] w-[48px] border border-solid border-[#858585] rounded-[4px]"
+            />
+            <InputOTPSlot
+              index={2}
+              className="text-h1 h-[64px] w-[48px] border border-solid border-[#858585] rounded-[4px]"
+            />
+            <InputOTPSlot
+              index={3}
+              className="text-h1 h-[64px] w-[48px] border border-solid border-[#858585] rounded-[4px]"
+            />
+            <InputOTPSlot
+              index={4}
+              className="text-h1 h-[64px] w-[48px] border border-solid border-[#858585] rounded-[4px]"
+            />
+            <InputOTPSlot
+              index={5}
+              className="text-h1 h-[64px] w-[48px] border border-solid border-[#858585] rounded-[4px]"
+            />
+          </InputOTPGroup>
+        </InputOTP>
       </DialogContent>
     </Dialog>
   );
