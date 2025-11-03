8c099e4c90d37b40cedeabade2a06868867206f6
Gavin Normand <75314990+gavinnormand@users.noreply.github.com>
2025-11-03 17:55:00 -0500

    Migration to Resend Email Service (#272)
    * Migration to Resend Done Tests Updated
    
    * Try to fix github tests
    
    * Resend fix test issue
    
    * fix?
    
    * fix?
    
    * Changed error from printing to throwing exception

--- packages/email/src/index.ts
+++ packages/email/src/index.ts
@@ -2,7 +2,7 @@ import { env } from "@good-dog/env";
 
 import { EmailService } from "./service";
 
-export const emailService = new EmailService(env.MAILERSEND_API_KEY);
+export const emailService = new EmailService(env.RESEND_API_KEY);
 
 export type { EmailMessage } from "./service";
 export { EmailService } from "./service";
