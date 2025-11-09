056763d138f160901379c31694904f949e864015
Lauren Brissette <168485735+laurenbrissette@users.noreply.github.com>
2025-11-09 16:25:41 -0500

    correct state indicators for music

--- packages/components/src/landing-pages/MusicianLanding.tsx
+++ packages/components/src/landing-pages/MusicianLanding.tsx
@@ -6,17 +6,19 @@ import type { ReactNode } from "react";
 import People from "../svg/People";
 import MusicNote from "../svg/MusicNote";
 import StatusIndicator from "../base/StatusIndicator";
+import { MatchState } from "@good-dog/db";
 
 export default function MusicianLanding() {
-  const [data] = trpc.music.useSuspenseQuery();
+  const [data] = trpc.userMusic.useSuspenseQuery();
   return (
     <div className="flex flex-col gap-[32px] align-start w-full">
       <Header
         title={"Song Submissions"}
         subtitle={"This is where you view and manage your song submissions"} requestPath={"/music"}      />
       <div className="flex flex-wrap justify-start gap-4 mx-auto max-w-fit pb-[36px]">
-        {data.music.map((req, key) => (
-          <Card
+        {data.music.map((req, key) => {
+          const actionNeeded = !!req.matches.find((match) => match.matchState === MatchState.SONG_REQUESTED);
+          return (<Card
             title={req.songName}
             subheader={req.createdAt.toLocaleDateString("en-US", {
               year: "numeric",
@@ -29,13 +31,13 @@ export default function MusicianLanding() {
                   <Line text={req.performerName} icon={<People />} />
                   <Line text={req.genre} icon={<MusicNote />} />
                 </div>
-                <StatusIndicator variant={"warning"} text={"Song submitted"} />
+                <StatusIndicator variant={actionNeeded ? "error" : "success"} text={actionNeeded ? "Action needed" : "Song submitted"} />
               </div>
             }
             size={"small"}
             key={key}
-          />
-        ))}
+          />)
+})}
       </div>
     </div>
   );
