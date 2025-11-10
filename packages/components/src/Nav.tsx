9344e9529a79024aeb3a28e91def30396c55e86b
Wesley Tran <wesley.tran24@gmail.com>
2025-11-10 17:24:26 -0500

    Navbar changes (#285)
    * fixed the bug with the navlogo that was making it transparent. Outer cirlce now takes on the text-good-dog-main from parent
    
    * finsihed profile dropdown
    
    * Forgot password flow (#270)
    
    * Forgot password form
    
    * reset password initial layout
    
    * lint + error validation
    
    * update tests
    
    * add modals for transitions
    
    * cleanup comments + remove celadon
    
    * comment responses - graphics, eliminate extra useEffect and useStates
    
    * merge conflicts
    
    * merge conflicts
    
    * finished with the nav bar and as such, the profile icon
    
    * fix linting
    
    * fixed two small comments on PR. shoutout JP
    
    ---------
    
    Co-authored-by: Lauren Brissette <168485735+laurenbrissette@users.noreply.github.com>

--- packages/components/src/Nav.tsx
+++ packages/components/src/Nav.tsx
@@ -5,21 +5,19 @@ import Link from "next/link";
 import { trpc } from "@good-dog/trpc/client";
 
 import NavLogo from "./svg/NavLogo";
+import ProfileDropdown from "./base/ProfileDropdown";
 
 export default function Nav() {
   const [user] = trpc.user.useSuspenseQuery();
-  const signOutMutation = trpc.signOut.useMutation({
-    onSuccess: () => {
-      window.location.replace("/");
-    },
-  });
 
   return (
     <header className="w-full text-good-dog-main">
       <div className="flex items-center justify-between bg-transparent">
         <Link href="/" className="flex items-center gap-3">
           <NavLogo />
-          <p className="font-righteous text-2xl">GOOD DOG LICENSING</p>
+          <p className="font-righteous text-good-dog-main text-2xl">
+            GOOD DOG LICENSING
+          </p>
         </Link>
 
         <nav className="flex items-center gap-8 text-lg">
@@ -31,18 +29,7 @@ export default function Nav() {
           </Link>
           {user ? (
             <>
-              <Link
-                href="/dashboard"
-                className="underline-offset-4 transition hover:underline"
-              >
-                Account
-              </Link>
-              <button
-                onClick={() => signOutMutation.mutate()}
-                className="underline-offset-4 hover:underline"
-              >
-                Logout
-              </button>
+              <ProfileDropdown />
             </>
           ) : (
             <Link href="/login" className="underline-offset-4 hover:underline">
