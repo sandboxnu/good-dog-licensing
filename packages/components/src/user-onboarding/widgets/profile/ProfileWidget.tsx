82ee688d1c03fd4504929b5d3bbf5bcb3b48e201
WesleyTran0 <wesley.tran24@gmail.com>
2025-11-16 16:29:29 -0500

    still templating

--- packages/components/src/user-onboarding/widgets/profile/ProfileWidget.tsx
+++ packages/components/src/user-onboarding/widgets/profile/ProfileWidget.tsx
@@ -6,17 +6,31 @@ export default function ProfileWidget() {
     <div className="flex flex-col gap-[24px] w-[752px]">
       <div>pfp + name + role</div>
       <div className="flex flex-col gap-y-[16px]">
-        <div>personal details</div>
-        <div className="rounded-2xl">
-          <header className="bg-gray-200">Security</header>
-          <div className="rounded-2xl">
-            <div>email</div>
-            <div>password</div>
+        <div className="rounded-2xl bg-white border">
+          <header className="flex bg-gray-200 rounded-t-2xl pb-[12px] pt-[13px] pl-[23.5]">
+            Personal details
+            <Button size="medium" variant="outlined" />
+          </header>
+          <div className="flex flex-col gap-y-[16px] rounded-2xl p-[24px] pt-[16px]">
+            woooo
+          </div>
+        </div>
+        <div className="rounded-2xl bg-white border">
+          <header className="bg-gray-200 rounded-t-2xl pb-[12px] pt-[13px] pl-[23.5]">
+            Security
+          </header>
+          <div className="flex flex-col gap-y-[16px] rounded-2xl p-[24px] pt-[16px]">
+            <div className="text-gray-400">email</div>
+            <div>actual sensitive email info</div>
+            <div>
+              <div className="text-gray-400">password</div>
+              <div>actual sensitive password info</div>
+            </div>
           </div>
         </div>
 
         <div className="rounded-2xl bg-white border">
-          <header className="bg-gray w-full self-stretch pb-[12px] pt-[13px] pl-[23.5px] text-error">
+          <header className="bg-gray pb-[12px] pt-[13px] pl-[23.5px] text-error">
             Delete account
           </header>
           <div className="flex flex-col gap-y-[16px] rounded-2xl p-[24px] pt-[16px]">
