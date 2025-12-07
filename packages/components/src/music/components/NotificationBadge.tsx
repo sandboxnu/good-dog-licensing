76f04d4fdc5e8900147a6c602d0059fe16727997
gavinnormand <normandgavin@gmail.com>
2025-12-06 23:08:36 -0500

    Try to fix error

--- packages/components/src/music/components/NotificationBadge.tsx
+++ packages/components/src/music/components/NotificationBadge.tsx
@@ -1,13 +1,9 @@
 export function NotificationBadge({ number }: { number: number }) {
   return (
-    <div>
-      {number > 0 && (
-        <div className="rounded-full w-5 h-5 bg-error">
-          <div className="bg-good-dog-accent text-white text-xs font-semibold w-5 h-5 flex items-center justify-center rounded-full">
-            {number}
-          </div>
-        </div>
-      )}
+    <div className="rounded-full w-5 h-5 bg-error">
+      <div className="bg-good-dog-accent text-white text-xs font-semibold w-5 h-5 flex items-center justify-center rounded-full">
+        {number}
+      </div>
     </div>
   );
 }
