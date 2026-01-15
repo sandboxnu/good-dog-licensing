d81bc585521f3e5b134abaec805d316f5450a449
WesleyTran0 <wesley.tran24@gmail.com>
2026-01-14 19:59:37 -0500

    Mid way through the front end design for profile page.
    - Modal for email flows probably ok
    - Modal for delete needs to be done
    - Input areas for personal details are half done
    - All procedures need to be added

--- packages/components/src/user-onboarding/widgets/profile/ProfileWidget.tsx
+++ packages/components/src/user-onboarding/widgets/profile/ProfileWidget.tsx
@@ -5,7 +5,14 @@ import { useRouter } from "next/navigation";
 import Button from "../../../base/Button";
 import ErrorExclamation from "../../../svg/status-icons/ErrorExclamation";
 import ProfileIcon from "../../../svg/ProfileIcon";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import SetEmailModal from "./SetEmailModal";
+import { FormProvider, useForm } from "react-hook-form";
+import z from "zod";
+import { zSignUpValues } from "@good-dog/trpc/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import EmailCodeModal from "../sign-up-widget/EmailCodeModal";
+import RHFTextInput from "../../../rhf-base/RHFTextInput";
 
 function InfoField({ header, content }: { header: string; content: string }) {
   return (
@@ -16,9 +23,15 @@ function InfoField({ header, content }: { header: string; content: string }) {
   );
 }
 
+type SignUpFormFields = z.input<typeof zSignUpValues>;
+
 export default function ProfileWidget() {
   const router = useRouter();
-  const { data: user } = trpc.user.useQuery();
+  const { data: user, isLoading } = trpc.user.useQuery();
+
+  const formMethods = useForm<SignUpFormFields>({
+    resolver: zodResolver(zSignUpValues),
+  });
 
   const userRoleFormatted = user
     ? user.role.charAt(0).toUpperCase() + user.role.slice(1).toLowerCase()
@@ -31,100 +44,234 @@ export default function ProfileWidget() {
     : "";
 
   useEffect(() => {
-    if (!user) {
+    if (!isLoading && !user) {
       router.push("/login");
     }
-  }, [user, router]);
+  }, [user, isLoading, router]);
+
+  const [editingPersonalDetails, setEditingPersonalDetails] = useState(false);
+  const [displaySetEmailModal, setDisplaySetEmailModal] = useState(false); // which email to change to
+  const [changeEmailError, setChangeEmailError] = useState(false);
+  const [displayEmailCodeModal, setDisplayEmailCodeModal] = useState(false); // code verification
+  const [submitEmailCodeError, setSubmitEmailCodeError] = useState(false);
+
+  // doesn't work since user is logged in and there the procedure uses a not auth'd template
+  const sendEmailVerificationMutation = trpc.sendEmailVerification.useMutation({
+    onSuccess: () => {
+      setDisplaySetEmailModal(false);
+      setDisplayEmailCodeModal(true);
+    },
+    onError: () => {
+      setChangeEmailError(true);
+    },
+  });
+
+  const verifyEmailCodeMutation = trpc.verifyEmailCode.useMutation({
+    onSuccess: () => {
+      setDisplayEmailCodeModal(false);
+      setSubmitEmailCodeError(false);
+    },
+    onError: () => {
+      setSubmitEmailCodeError(true);
+    },
+  });
+
+  const verifyEmailCode = (code: string) => {
+    formMethods.setValue("emailCode", code);
+    verifyEmailCodeMutation.mutate({
+      email: formMethods.watch("email"),
+      emailCode: code,
+    });
+  };
+
+  const handleVerifyEmail = async () => {
+    sendEmailVerificationMutation.reset();
+    const initialSignUpInfoIsValid = await formMethods.trigger(["email"]);
+    if (initialSignUpInfoIsValid) {
+      sendEmailVerificationMutation.mutate({
+        email: formMethods.watch("email"),
+      });
+    }
+  };
 
   return (
     <div className="flex flex-col gap-6 w-[752px]">
-      <div className="flex flex-row items-center gap-4">
-        <ProfileIcon color="light" size={56} />
-        <div className="flex flex-col">
-          <header className="text-good-dog-main text-xl font-semibold">
-            {user?.firstName + " " + user?.lastName}
-          </header>
-          <div className="text-dark-gray-200">
-            {userRoleFormatted} | Since {userCreatedAtFormatted}
-          </div>
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-y-[16px]">
-        <div className="rounded-2xl bg-white border">
-          <header className="flex justify-between items-center bg-gray-200 rounded-t-2xl pb-[12px] pt-[13px] px-[24px]">
-            Personal details
-            <Button
-              size="small"
-              variant="outlined"
-              displayIcon="pencil"
-              label="Edit"
-            />
-          </header>
-          <div className="flex flex-col gap-y-[16px] rounded-2xl p-[24px] pt-[16px]">
-            <div className="flex flex-row ">
-              <div className="w-[380px]">
-                <InfoField
-                  header="First name"
-                  content={user ? user.firstName : ""}
-                />
-              </div>
-              <InfoField
-                header="Last name"
-                content={user ? user.lastName : ""}
-              />
-            </div>
-            <div className="flex flex-row ">
-              <div className="w-[380px]">
-                <InfoField
-                  header="Group"
-                  content={user?.affiliation ? user.affiliation : "NONE"}
-                />
-              </div>
-              <InfoField
-                header="IPI No."
-                content={user?.ipi ? user.ipi : "NONE"}
-              />
-            </div>
-          </div>
-        </div>
-        <div className="rounded-2xl bg-white border">
-          <header className="bg-gray-200 rounded-t-2xl pb-[12px] pt-[13px] px-[24px]">
-            Security
-          </header>
-          <div className="flex flex-col gap-y-[16px] rounded-2xl p-[24px] pt-[16px]">
-            <div className="flex flex-row justify-between items-center">
-              <InfoField header="Email" content={user ? user.email : ""} />
-              <Button label="Change email" size="small" variant="outlined" />
-            </div>
-            <div className="flex flex-row justify-between items-center">
-              <InfoField header="Password" content="**********" />
-              <Button label="Change password" size="small" variant="outlined" />
+      <FormProvider {...formMethods}>
+        <SetEmailModal
+          isOpen={displaySetEmailModal}
+          close={() => setDisplaySetEmailModal(false)}
+          onVerifyEmail={handleVerifyEmail}
+          resendEmail={handleVerifyEmail} // a little out of place..
+          emailAlreadyExists={false}
+          error={changeEmailError}
+        />
+        <EmailCodeModal
+          isOpen={displayEmailCodeModal}
+          close={() => setDisplayEmailCodeModal(false)}
+          email={formMethods.watch("email")}
+          verifyCode={verifyEmailCode}
+          resendEmail={handleVerifyEmail}
+          codeIsWrong={submitEmailCodeError}
+        />
+        <div className="flex flex-row items-center gap-4">
+          <ProfileIcon color="light" size={56} />
+          <div className="flex flex-col">
+            <header className="text-good-dog-main text-xl font-semibold">
+              {user?.firstName + " " + user?.lastName}
+            </header>
+            <div className="text-dark-gray-200">
+              {userRoleFormatted} | Since {userCreatedAtFormatted}
             </div>
           </div>
         </div>
 
-        <div className="rounded-2xl bg-white border">
-          <header className="rounded-t-2xl bg-white pb-[12px] pt-[13px] px-[23.5px] text-error">
-            Delete account
-          </header>
-          <div className="flex flex-col gap-y-[16px] rounded-2xl p-[24px] pt-[16px]">
-            <div className="flex flex-row justify-left items-center text-dark-gray-300">
-              <ErrorExclamation size="medium" /> This will permanently delete
-              your account and all your information. This action can't be
-              undone!
+        <div className="flex flex-col gap-y-[16px]">
+          <div className="rounded-2xl bg-white border">
+            <header className="flex justify-between items-center bg-gray-200 rounded-t-2xl py-2.5 px-[24px]">
+              <p className="text-lg font-medium text-good-dog-main">
+                Personal details
+              </p>
+
+              {editingPersonalDetails ? (
+                // TODO: look at figma for the spacing / button designs
+                <div className="flex flex-row gap-3">
+                  <Button
+                    size="small"
+                    variant="contained"
+                    label="Save"
+                    onClick={() => console.log("save but not implemented yet")}
+                  />
+                  <Button
+                    size="small"
+                    variant="outlined"
+                    label="Cancel"
+                    onClick={() => setEditingPersonalDetails(false)}
+                  />
+                </div>
+              ) : (
+                <Button
+                  size="small"
+                  variant="outlined"
+                  displayIcon="pencil"
+                  label="Edit"
+                  onClick={() => setEditingPersonalDetails(true)}
+                />
+              )}
+            </header>
+            <div className="flex flex-col gap-y-[16px] rounded-2xl p-[24px] pt-[16px]">
+              {editingPersonalDetails ? (
+                <>
+                  <div className="flex flex-row ">
+                    <div className="w-[380px]">
+                      <RHFTextInput<SignUpFormFields>
+                        rhfName={"firstName"}
+                        label={"First name"}
+                        placeholder={user ? user.firstName : ""}
+                        id={"firstName"}
+                      />
+                    </div>
+                    <RHFTextInput<SignUpFormFields>
+                      rhfName={"lastName"}
+                      label={"Last Name"}
+                      placeholder={user ? user.lastName : ""}
+                      id={"lastName"}
+                    />
+                  </div>
+                  <div className="flex flex-row ">
+                    <div className="w-[380px]">
+                      {/* TODO: Drop down + group isn't in signupformfield */}
+                      {/* <RHFTextInput<SignUpFormFields> */}
+                      {/*   rhfName={"Group"}  */}
+                      {/*   label={"Last Name"} */}
+                      {/*   placeholder={user ? user.lastName : ""} */}
+                      {/*   id={"lastName"} */}
+                      {/* /> */}
+                    </div>
+                    <RHFTextInput<SignUpFormFields>
+                      rhfName={"ipi"}
+                      label={"IPI No."}
+                      placeholder={user?.ipi ? user.ipi : "NONE"}
+                      id={"ipi"}
+                    />
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="flex flex-row ">
+                    <div className="w-[380px]">
+                      <InfoField
+                        header="First name"
+                        content={user ? user.firstName : ""}
+                      />
+                    </div>
+                    <InfoField
+                      header="Last name"
+                      content={user ? user.lastName : ""}
+                    />
+                  </div>
+                  <div className="flex flex-row ">
+                    <div className="w-[380px]">
+                      <InfoField
+                        header="Group"
+                        content={user?.affiliation ? user.affiliation : "NONE"}
+                      />
+                    </div>
+                    <InfoField
+                      header="IPI No."
+                      content={user?.ipi ? user.ipi : "NONE"}
+                    />
+                  </div>
+                </>
+              )}
             </div>
-            <div>
-              <Button
-                label="Delete account"
-                size="small"
-                variant="outlined"
-                error={true}
-              />
+          </div>
+          <div className="rounded-2xl bg-white border">
+            <header className="text-lg font-medium text-good-dog-main bg-gray-200 rounded-t-2xl py-2.5 px-[24px]">
+              Security
+            </header>
+            <div className="flex flex-col gap-y-[16px] rounded-2xl p-[24px] pt-[16px]">
+              <div className="flex flex-row justify-between items-center">
+                <InfoField header="Email" content={user ? user.email : ""} />
+                <Button
+                  label="Change email"
+                  size="small"
+                  variant="outlined"
+                  onClick={() => setDisplaySetEmailModal(true)}
+                />
+              </div>
+              <div className="flex flex-row justify-between items-center">
+                <InfoField header="Password" content="**********" />
+                <Button
+                  label="Change password"
+                  size="small"
+                  variant="outlined"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="rounded-2xl bg-white border">
+            <header className="rounded-t-2xl bg-white py-2.5 px-[23.5px] text-error font-medium">
+              Delete account
+            </header>
+            <div className="flex flex-col gap-y-[16px] rounded-2xl p-[24px] pt-[16px]">
+              <div className="flex flex-row justify-left items-center text-dark-gray-300">
+                <ErrorExclamation size="medium" /> This will permanently delete
+                your account and all your information. This action can't be
+                undone!
+              </div>
+              <div>
+                <Button
+                  label="Delete account"
+                  size="small"
+                  variant="outlined"
+                  error={true}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </FormProvider>
     </div>
   );
 }
---
+++ packages/components/src/user-onboarding/widgets/profile/SetEmailModal.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import type z from "zod";
+import Modal from "../../../base/Modal";
+
+import { zSignUpValues } from "@good-dog/trpc/schema";
+import RHFTextInput from "../../../rhf-base/RHFTextInput";
+import { useFormContext } from "react-hook-form";
+import Button from "../../../base/Button";
+
+interface SetEmailModalProps {
+  isOpen: boolean;
+  close: () => void;
+  onVerifyEmail: () => void;
+  resendEmail: () => void;
+  emailAlreadyExists: boolean;
+  error: boolean;
+}
+
+type SignUpFormFields = z.input<typeof zSignUpValues>;
+
+export default function SetEmailModal({
+  isOpen,
+  close,
+  onVerifyEmail,
+  resendEmail,
+  emailAlreadyExists,
+  error,
+}: SetEmailModalProps) {
+  const {
+    formState: { errors },
+  } = useFormContext<SignUpFormFields>();
+
+  return (
+    <div className="flex flex-col">
+      <Modal
+        open={isOpen}
+        onClose={close}
+        headerText="Set new email"
+        width={500}
+        height={322}
+      >
+        <div className="w-3/4 pt-4">
+          <RHFTextInput<SignUpFormFields>
+            rhfName="email"
+            label="Email"
+            placeholder="example@email.com"
+            id="email"
+            errorText={
+              emailAlreadyExists
+                ? "User with email already exists"
+                : errors.email?.message
+            }
+            required
+          />
+        </div>
+        <div className="flex flex-row flex-wrap items-center justify-center space-x-1 py-4">
+          <p className="text-body2">Didn't get an email?</p>
+          <button
+            onClick={() => {
+              resendEmail();
+            }}
+            type="button"
+            className="text-body2 font-medium text-secondary underline"
+          >
+            Resend
+          </button>
+        </div>
+        <Button
+          label="Verify email"
+          size="small"
+          variant="contained"
+          onClick={onVerifyEmail}
+        ></Button>
+      </Modal>
+    </div>
+  );
+}
