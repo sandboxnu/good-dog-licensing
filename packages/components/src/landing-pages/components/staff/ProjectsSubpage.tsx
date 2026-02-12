524b0261a1ca0f2b7fa4aa1bb1fb0fff9f5bc7e5
Lauren Brissette <168485735+laurenbrissette@users.noreply.github.com>
2026-02-12 18:29:52 -0500

    prefetch admin and pnr data

--- packages/components/src/landing-pages/components/staff/ProjectsSubpage.tsx
+++ packages/components/src/landing-pages/components/staff/ProjectsSubpage.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState } from "react";
 import type { ProjectSubmissionWithSongRequestAndMatches } from "../../../../utils/getStatusFromProject";
 import getStatusFromProject from "../../../../utils/getStatusFromProject";
--- packages/components/src/landing-pages/components/staff/SideBar.tsx
+++ packages/components/src/landing-pages/components/staff/SideBar.tsx
@@ -18,7 +18,7 @@ export default function SideBar({
   isAdminView: boolean;
 }) {
   return (
-    <div className="flex bg-gray-100 bg-dark-gray-600 flex-1 w-[207px] h-[776px] pt-[32px] pl-[16px] pr-[16px] shadow-[0_2px_6px_0_#ECE6DF] rounded-[24px] h-[40px]">
+    <div className="flex bg-gray-100 dark:bg-dark-gray-600 flex-1 w-[207px] h-[776px] pt-[32px] pl-[16px] pr-[16px] shadow-[0_2px_6px_0_#ECE6DF] rounded-[24px] h-[40px]">
       <div className="flex flex-col gap-[4px]">
         <SideBarEntry
           active={activeTab === "submissions"}
