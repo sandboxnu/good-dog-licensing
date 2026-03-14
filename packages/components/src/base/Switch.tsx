4d95b09fa38520915f614f30715eefdba198df84
Lauren Brissette <168485735+laurenbrissette@users.noreply.github.com>
2026-03-14 15:10:06 -0400

    dark colors

--- packages/components/src/base/Switch.tsx
+++ packages/components/src/base/Switch.tsx
@@ -9,7 +9,7 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "group relative inline-flex h-[35px] w-[100px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-green-400 data-[state=unchecked]:bg-gray-300",
+      "group relative inline-flex h-[35px] w-[100px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-green-400  dark:data-[state=checked]:bg-green-300 data-[state=unchecked]:bg-gray-300",
       className,
     )}
     {...props}
@@ -17,7 +17,7 @@ const Switch = React.forwardRef<
   >
     <span
       aria-hidden="true"
-      className="pointer-events-none absolute left-5 top-1/2 -translate-y-1/2 text-xs font-[14px] text-cream-100 hidden group-data-[state=checked]:block"
+      className="pointer-events-none absolute left-5 top-1/2 -translate-y-1/2 text-xs font-[14px] text-cream-100 dark:text-mint-200 hidden group-data-[state=checked]:block"
     >
       ACTIVE
     </span>
@@ -29,7 +29,7 @@ const Switch = React.forwardRef<
     </span>
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none block h-[24px] w-[24px] rounded-full data-[state=checked]:bg-cream-100 data-[state=unchecked]:bg-dark-gray-300 shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-[69px] data-[state=unchecked]:translate-x-1",
+        "pointer-events-none block h-[24px] w-[24px] rounded-full data-[state=checked]:bg-cream-100 dark:data-[state=checked]:bg-mint-200 data-[state=unchecked]:bg-dark-gray-300 shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-[69px] data-[state=unchecked]:translate-x-1",
       )}
     />
   </SwitchPrimitives.Root>
