285abe72b0c8dddc3ff99649c5a8c4766b5bffa3
Amoli Patel <patel.amol@northeastern.edu>
2026-03-15 14:59:27 -0400

    Add return to sign in option on temp terms and privacy page

--- apps/web/app/(pages)/(public)/(logged-out-only)/terms/page.tsx
+++ apps/web/app/(pages)/(public)/(logged-out-only)/terms/page.tsx
@@ -1,8 +1,16 @@
 export default function TermsPage() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div className="flex flex-col h-screen items-center justify-center">
       <p className="text-center text-lg text-green-500">
         Full Terms and Privacy coming soon.
+      </p>
+      <p className="text-center text-lg text-green-500">
+        <a
+          href="/signup"
+          className="text-blue-500 hover:underline"
+        >
+          <u>Go back to sign up page.</u>
+        </a>
       </p>
     </div>
   );
