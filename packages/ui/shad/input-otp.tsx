d6425bd8d77e8cc821e6091d6c93a3aa65478755
Jordan Praissman <jordanpraissman@gmail.com>
2026-03-25 11:15:46 -0400

    Fix linting part 2

--- packages/ui/shad/input-otp.tsx
+++ packages/ui/shad/input-otp.tsx
@@ -35,9 +35,6 @@ const InputOTPSlot = React.forwardRef<
   const inputOTPContext = React.useContext(OTPInputContext);
 
   const slot = inputOTPContext.slots[index];
-  if (!slot) {
-    return null;
-  }
   const { char, hasFakeCaret, isActive } = slot;
 
   return (
@@ -45,7 +42,7 @@ const InputOTPSlot = React.forwardRef<
       ref={ref}
       className={cn(
         "relative flex h-9 w-9 items-center justify-center border-y border-r border-input text-sm shadow-sm transition-all first:rounded-l-md first:border-l last:rounded-r-md",
-        isActive && "z-10 ring-1 ring-ring",
+        Boolean(isActive) && "z-10 ring-1 ring-ring",
         className,
       )}
       {...props}
