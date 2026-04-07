74e791b165b40f770b062fd1dd90b19c7eba22b6
Wesley Tran <wesley.tran24@gmail.com>
2026-04-07 00:19:46 -0400

    MatchDrawer on Admin Dashboard (#453)
    * initial designs pretty well matched
    
    * prototype for the status indicators but otherwise all done
    
    * fix linting
    
    * Remaining email notifications (#445)
    
    * All emails and tests
    
    * Fix comment headers
    
    * fix formatting
    
    * Fix tests
    
    * Fix artist joining notification
    
    * remove getAllPNREmails method
    
    * Fix links and spacing betweeen names in email
    
    * Fixed button text, links, and other misc items
    
    * fix tests
    
    * Update seed script for live demo (#447)
    
    * Update seed script for live demo
    
    * Update
    
    * Reduce num of moderators too
    
    * Normalize Email + Links open in new tab (#451)
    
    * Normalize email
    
    * Add target blanks
    
    * Invite users (#452)
    
    * base of invite users modal for inviting pnrs to the site
    
    * style updates
    
    * fix lint
    
    * fix key error
    
    * lowercase emails
    
    * Only send the extra emails when in production (#454)
    
    * Login and signup design spec changes (#407)
    
    * Updated wording on initial sign up page
    
    * Update Nav Bar links to be underlined when selected
    
    * Add a 'back to login' option on the Forgot Password page
    
    * Make small styling changes to Login Page
    
    * Make small styling changes to Final Signup Page
    
    * Added 'How did you hear about GDL' multi-select
    
    * Add bottom padding to Email Code Modal when inputted code is wrong
    
    * Add Terms and Privacy checkbox to final sign up page
    
    * Fix form border on ginal sign up page
    
    * Fix final sign up form border again
    
    * Add dark mode styling to email modal
    
    * Ran format fix
    
    * Fix email modal so that code and error message go away once closed
    
    * Increase height of email modal
    
    * Fix email modal functionality and fix tests
    
    * Fix tests
    
    * Revert "Fix tests"
    
    This reverts commit ea0025890df1ebfb05e2e914a7c1e7335c43631b.
    
    * Fixed tests
    
    * Run bun format again
    
    * Fix nav bar linking to home page
    
    * Make howHeardAboutUs values more dynamic
    
    * Fix typecheck issues
    
    * Fix sign up flow + add placeholder to terms and privacy
    
    * Add return to sign in option on temp terms and privacy page
    
    * Store How Heard About Us in backend
    
    * Add functionality to hide/show password
    
    * Add checkbox to Terms and Service
    
    * Ran format fix
    
    * Add How Heard About Us to enumLabelMapper
    
    * Fix typecheck issues
    
    * Move terms and conditions page so all users can see
    
    * Open terms and privacy in a new tab
    
    * Use homelink variable in Nav
    
    * Make HowHeardAboutUs be an array and update tests accordingly
    
    * Add filters to the search modal when suggesting songs for a project (#456)
    
    * first try of implementing the new modal
    
    * revert changes on multi-select component that might affect the rest of the site
    
    * dark mode the buttons
    
    * added searchbar into the filtering options
    
    * finally can scroll
    
    * small color change
    
    * selected tags only show when there are filters selected to avoid weird spacing
    
    * fixed weird shadow thing with the search bar
    
    * removed StatusIndicatorDetails and abstracted it into StatusIndicator
    
    * fixing up the styling of the indicator
    
    ---------
    
    Co-authored-by: Gavin Normand <75314990+gavinnormand@users.noreply.github.com>
    Co-authored-by: Jordan Praissman <90515336+jpraissman@users.noreply.github.com>
    Co-authored-by: Lauren Brissette <168485735+laurenbrissette@users.noreply.github.com>
    Co-authored-by: Amoli Patel <patel.amol@northeastern.edu>

--- packages/components/src/Nav.tsx
+++ packages/components/src/Nav.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 import { trpc } from "@good-dog/trpc/client";
 
--- packages/components/src/matching/components/User.tsx
+++ packages/components/src/matching/components/User.tsx
@@ -6,7 +6,7 @@ export default function User({ name }: { name: string }) {
   return (
     <div className="flex w-fit flex-row gap-2 rounded-xl border-[0.5px] border-cream-400 bg-cream-100 p-2 shadow-md dark:border-black dark:bg-green-400">
       <ProfileIcon color="light" size={20} name={name} />
-      <p className="text-sm dark:text-gray-200">{name}</p>
+      <p className="text-sm text-dark-gray-500 dark:text-gray-300">{name}</p>
     </div>
   );
 }
