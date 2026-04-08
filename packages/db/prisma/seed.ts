3b92325dbbf50d1c7df9f5a47d3e07daf5c9a47f
Jordan Praissman <90515336+jpraissman@users.noreply.github.com>
2026-04-08 18:13:05 -0400

    Revert back seed script (#462)

--- packages/db/prisma/seed.ts
+++ packages/db/prisma/seed.ts
@@ -236,7 +236,7 @@ async function main() {
   // Generate song requests
 
   const songRequestCreations = createdProjects.flatMap((project) => {
-    const requestCount = Math.floor(Math.random() * 4);
+    const requestCount = Math.floor(Math.random() * 5);
     return generateSongRequests(project.projectId, requestCount).map(
       (songRequest) =>
         prisma.songRequest.create({
