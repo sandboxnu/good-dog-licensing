1967532358aa75b8db75e9f54c6fc0f1c629d0cf
Jordan Praissman <jordanpraissman@gmail.com>
2026-04-10 16:54:05 -0400

    FAQSection

--- packages/components/src/landing-pages/general/components/ContributorsSection.tsx
+++ packages/components/src/landing-pages/general/components/ContributorsSection.tsx
@@ -9,60 +9,63 @@ export default function ContributorsSection() {
     <GreenOval className="flex items-center justify-center">
       <div className={clsx(CONTENT_MAX_WIDTH_CLASSES.small)}>
         <div className="relative pb-[120px] pt-[120px]">
-          <h2 className="text-left text-[28px] font-medium not-italic leading-[96%] text-mint-200 md:text-[48px]">
-            Our contributors
-          </h2>
-          <p className="text-left text-[16px] font-normal not-italic leading-normal text-gray-100 md:text-[20px]">
-            Discover the people driving our platform, uniting strategy,
-            creativity, and music.
-          </p>
+          <div className="py-[50px] md:py-[0px]">
+            <h2 className="text-left text-[35px] font-medium not-italic leading-[96%] text-mint-200 md:text-[48px]">
+              Our contributors
+            </h2>
+            <p className="text-left text-[18px] font-normal not-italic leading-normal text-gray-100 md:text-[20px]">
+              Discover the people driving our platform, uniting strategy,
+              creativity, and music.
+            </p>
 
-          <div className="mt-[36px] flex flex-col gap-[56px] md:flex-row">
-            <div className="flex flex-1 flex-col items-center gap-[10px] self-stretch rounded-[24px] border border-cream-400 bg-cream-100 p-[24px] shadow-card-light dark:border-cream-600 dark:bg-green-600 dark:shadow-[0_2px_6px_0_#2E2E2E]">
-              <div className="h-[250px] w-[250px] overflow-hidden rounded-[136px] shadow-[0_2px_6px_0_#BFBCB8]">
-                <img
-                  src="/images/profHeadshot.png"
-                  alt="Founder"
-                  className="h-[250px] w-[250px] shadow-[0_2px_6px_0_#BFBCB8]"
-                />
+            <div className="mt-[36px] flex flex-col gap-[56px] md:flex-row">
+              <div className="flex flex-1 flex-col items-center gap-[10px] self-stretch rounded-[24px] border border-cream-400 bg-cream-100 p-[24px] shadow-card-light dark:border-cream-600 dark:bg-green-600 dark:shadow-[0_2px_6px_0_#2E2E2E]">
+                <div className="h-[250px] w-[250px] overflow-hidden rounded-[136px] shadow-[0_2px_6px_0_#BFBCB8]">
+                  <img
+                    src="/images/profHeadshot.png"
+                    alt="Founder"
+                    className="h-[250px] w-[250px] shadow-[0_2px_6px_0_#BFBCB8]"
+                  />
+                </div>
+                <div className="mt-[8px] text-center text-[35px] font-medium not-italic leading-[104%] text-green-400 dark:text-mint-200 md:text-[40px]">
+                  Founder
+                </div>
+                <div className="mt-[16px] break-words text-left text-[18px] font-medium not-italic leading-[128%] text-dark-gray-500 dark:text-gray-300 md:text-[18px]">
+                  Professor Herlihy is a professor in both the College of Arts,
+                  Media, and Design at Northeastern, and at the Law School, runs
+                  his own entertainment law practice, and frequently writes and
+                  records original music. Students in Professor Herlihy's Music
+                  Licensing for Media course work with Good Dog Licensing
+                  throughout the semester, gaining hands-on experience in A&R,
+                  Music Supervisions
+                  <Footnote
+                    number={5}
+                    tooltip="Music supervision is the art of selecting and licensing preexisting songs or recordings for use in visual media like film, television, video games, and advertising."
+                  />
+                  , and Music Licensing.
+                </div>
               </div>
-              <div className="mt-[8px] text-center text-[28px] font-medium not-italic leading-[104%] text-green-400 dark:text-mint-200 md:text-[40px]">
-                Founder
-              </div>
-              <div className="mt-[16px] break-words text-left text-[16px] font-medium not-italic leading-[128%] text-dark-gray-500 dark:text-gray-300 md:text-[18px]">
-                Professor Herlihy is a professor in both the College of Arts,
-                Media, and Design at Northeastern, and at the Law School, runs
-                his own entertainment law practice, and frequently writes and
-                records original music. Students in Professor Herlihy's Music
-                Licensing for Media course work with Good Dog Licensing
-                throughout the semester, gaining hands-on experience in A&R,
-                Music Supervisions
-                <Footnote
-                  number={5}
-                  tooltip="Music supervision is the art of selecting and licensing preexisting songs or recordings for use in visual media like film, television, video games, and advertising."
-                />
-                , and Music Licensing.
-              </div>
-            </div>
-            <div className="flex flex-1 flex-col items-center gap-[10px] self-stretch rounded-[24px] border border-cream-400 bg-cream-100 p-[24px] shadow-card-light dark:border-cream-600 dark:bg-green-600 dark:shadow-[0_2px_6px_0_#2E2E2E]">
-              <div className="flex h-[250px] w-[250px] flex-col items-center justify-center overflow-hidden rounded-[136px] shadow-[0_2px_6px_0_#BFBCB8] dark:bg-mint-300">
-                <img
-                  src="/images/greenLineRecords.png"
-                  alt="Green Line Records"
-                  className="h-[178.6px] w-[224px]"
-                />
-              </div>
-              <div className="mt-[8px] text-center text-[28px] font-medium not-italic leading-[104%] text-green-400 dark:text-mint-200 md:text-[40px]">
-                Green Line Records
-              </div>
-              <div className="mt-[16px] break-words text-left text-[16px] font-medium not-italic leading-[128%] text-dark-gray-500 dark:text-gray-300 md:text-[18px]">
-                Green Line Records is Northeastern University's student-run
-                record label. Green Line Records aims to showcase Boston's music
-                scene by offering their artists a full range of services within
-                A&R, Recording, Events, and Creative Services departments. Good
-                Dog Licensing operates as Green Line Record's licensing
-                department, offering musicians and media makers the chance to
-                connect with Green Line Record and utilize their services.
+              <div className="flex flex-1 flex-col items-center gap-[10px] self-stretch rounded-[24px] border border-cream-400 bg-cream-100 p-[24px] shadow-card-light dark:border-cream-600 dark:bg-green-600 dark:shadow-[0_2px_6px_0_#2E2E2E]">
+                <div className="flex h-[250px] w-[250px] flex-col items-center justify-center overflow-hidden rounded-[136px] shadow-[0_2px_6px_0_#BFBCB8] dark:bg-mint-300">
+                  <img
+                    src="/images/greenLineRecords.png"
+                    alt="Green Line Records"
+                    className="h-[178.6px] w-[224px]"
+                  />
+                </div>
+                <div className="mt-[8px] text-center text-[35px] font-medium not-italic leading-[104%] text-green-400 dark:text-mint-200 md:text-[40px]">
+                  Green Line Records
+                </div>
+                <div className="mt-[16px] break-words text-left text-[18px] font-medium not-italic leading-[128%] text-dark-gray-500 dark:text-gray-300 md:text-[20px]">
+                  Green Line Records is Northeastern University's student-run
+                  record label. Green Line Records aims to showcase Boston's
+                  music scene by offering their artists a full range of services
+                  within A&R, Recording, Events, and Creative Services
+                  departments. Good Dog Licensing operates as Green Line
+                  Record's licensing department, offering musicians and media
+                  makers the chance to connect with Green Line Record and
+                  utilize their services.
+                </div>
               </div>
             </div>
           </div>
--- packages/components/src/landing-pages/general/components/FaqSection.tsx
+++ packages/components/src/landing-pages/general/components/FaqSection.tsx
@@ -8,18 +8,18 @@ import {
 export default function FaqSection() {
   return (
     <div>
-      <h3 className="text-left text-[28px] font-medium not-italic leading-[96%] text-dark-gray-500 dark:text-mint-300 md:text-[48px]">
+      <h3 className="text-left text-[35px] font-medium not-italic leading-[96%] text-dark-gray-500 dark:text-mint-300 md:text-[48px]">
         Frequently asked questions
       </h3>
       <Accordion multiple={false} className="mt-[36px] gap-[8px]">
         <AccordionItem
           value="faq-1"
-          className="rounded-[24px] border border-cream-400 bg-cream-100 px-[24px] shadow-card-light dark:border-[#454545] dark:bg-green-600 dark:shadow-[0_2px_6px_0_#454545]"
+          className="rounded-[24px] border border-cream-400 bg-cream-100 px-[24px] shadow-card-light dark:border-dark-gray-400 dark:bg-green-600 dark:shadow-[0_2px_6px_0_#454545]"
         >
-          <AccordionTrigger className="py-[24px] text-[20px] font-semibold text-green-400 dark:text-mint-200">
+          <AccordionTrigger className="py-[24px] text-[18px] font-semibold leading-[120%] text-green-400 dark:text-mint-200 md:text-[20px]">
             What are PROs and why should I register?
           </AccordionTrigger>
-          <AccordionContent className="pb-[24px] text-left text-[18px] font-medium text-dark-gray-500 dark:text-gray-300">
+          <AccordionContent className="pb-[24px] text-left text-[16px] font-medium text-dark-gray-500 dark:text-gray-300 md:text-[18px]">
             They are Performance Rights Organizations such as BMI and ASCAP. In
             this case, it&apos;s a great way to keep track of where the media
             that uses your song is being performed. When Media Makers submit cue
@@ -29,13 +29,13 @@ export default function FaqSection() {
         </AccordionItem>
         <AccordionItem
           value="faq-2"
-          className="rounded-[24px] border border-cream-400 bg-cream-100 px-[24px] shadow-card-light dark:border-[#454545] dark:bg-green-600 dark:shadow-[0_2px_6px_0_#454545]"
+          className="rounded-[24px] border border-cream-400 bg-cream-100 px-[24px] shadow-card-light dark:border-dark-gray-400 dark:bg-green-600 dark:shadow-[0_2px_6px_0_#454545]"
         >
-          <AccordionTrigger className="py-[24px] text-[20px] font-semibold text-green-400 dark:text-mint-200">
+          <AccordionTrigger className="py-[24px] text-[18px] font-semibold leading-[120%] text-green-400 dark:text-mint-200 md:text-[20px]">
             What if the media Good Dog wants to sync my song in isn&apos;t my
             vibe?
           </AccordionTrigger>
-          <AccordionContent className="pb-[24px] text-left text-[18px] font-medium text-dark-gray-500 dark:text-gray-300">
+          <AccordionContent className="pb-[24px] text-left text-[16px] font-medium text-dark-gray-500 dark:text-gray-300 md:text-[18px]">
             It&apos;s still a great way to reach a wider audience and get
             traction. But at the end of the day, if you don&apos;t want your
             music in a specific project, you get the last call!
@@ -43,13 +43,13 @@ export default function FaqSection() {
         </AccordionItem>
         <AccordionItem
           value="faq-3"
-          className="rounded-[24px] border border-cream-400 bg-cream-100 px-[24px] shadow-card-light dark:border-[#454545] dark:bg-green-600 dark:shadow-[0_2px_6px_0_#454545]"
+          className="rounded-[24px] border border-cream-400 bg-cream-100 px-[24px] shadow-card-light dark:border-dark-gray-400 dark:bg-green-600 dark:shadow-[0_2px_6px_0_#454545]"
         >
-          <AccordionTrigger className="py-[24px] text-[20px] font-semibold text-green-400 dark:text-mint-200">
+          <AccordionTrigger className="py-[24px] text-[18px] font-semibold leading-[120%] text-green-400 dark:text-mint-200 md:text-[20px]">
             How long before I need my project done should I reach out to Good
             Dog?
           </AccordionTrigger>
-          <AccordionContent className="pb-[24px] text-left text-[18px] font-medium text-dark-gray-500 dark:text-gray-300">
+          <AccordionContent className="pb-[24px] text-left text-[16px] font-medium text-dark-gray-500 dark:text-gray-300 md:text-[18px]">
             It&apos;s never too early! Reach out to us as early in the process
             as possible. When you&apos;re developing a story, let&apos;s have a
             conversation about the kind of music you need so we can get started.
@@ -58,13 +58,13 @@ export default function FaqSection() {
         </AccordionItem>
         <AccordionItem
           value="faq-4"
-          className="rounded-[24px] border border-cream-400 bg-cream-100 px-[24px] shadow-card-light dark:border-[#454545] dark:bg-green-600 dark:shadow-[0_2px_6px_0_#454545]"
+          className="rounded-[24px] border border-cream-400 bg-cream-100 px-[24px] shadow-card-light dark:border-dark-gray-400 dark:bg-green-600 dark:shadow-[0_2px_6px_0_#454545]"
         >
-          <AccordionTrigger className="py-[24px] text-[20px] font-semibold text-green-400 dark:text-mint-200">
+          <AccordionTrigger className="py-[24px] text-[18px] font-semibold leading-[120%] text-green-400 dark:text-mint-200 md:text-[20px]">
             I normally just use an MP3 converter to get whatever music I need
             from Youtube. Why should I stop?
           </AccordionTrigger>
-          <AccordionContent className="pb-[24px] text-left text-[18px] font-medium text-dark-gray-500 dark:text-gray-300">
+          <AccordionContent className="pb-[24px] text-left text-[16px] font-medium text-dark-gray-500 dark:text-gray-300 md:text-[18px]">
             That won&apos;t fly if you&apos;re submitting your work to festivals
             or if you get offered a theatrical license. License your music
             properly so that your work has legs, and learn about writing music
